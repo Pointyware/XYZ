bdbf8d08c235608a3d1ac9dc10380ddbc89e4b46
Taush Sampley <taushsampley@gmail.com>
2024-07-14 12:16:18 -0500

    Add loading background color

--- core/ui/src/commonMain/kotlin/design/Color.kt
+++ core/ui/src/commonMain/kotlin/design/Color.kt
@@ -2,6 +2,8 @@ package org.pointyware.xyz.core.ui.design
 
 import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.lightColorScheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
 
 internal val lightColors = lightColorScheme(
 
@@ -10,3 +12,5 @@ internal val lightColors = lightColorScheme(
 internal val darkColors = darkColorScheme(
 
 )
+
+val GrayedOut = Color(0x2F000000)
