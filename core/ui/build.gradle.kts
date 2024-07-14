3766d684dfe3184f1964378e7f59bc1bc5f3871f
Taush Sampley <taushsampley@gmail.com>
2024-07-14 12:00:42 -0500

    Enable compose resources for :core:ui

--- core/ui/build.gradle.kts
+++ core/ui/build.gradle.kts
@@ -43,6 +43,7 @@ kotlin {
 
                 implementation(compose.ui)
                 implementation(compose.material3)
+                implementation(compose.components.resources)
                 implementation(compose.components.uiToolingPreview) // fleet support
 
                 implementation(libs.kotlinx.dateTime)
