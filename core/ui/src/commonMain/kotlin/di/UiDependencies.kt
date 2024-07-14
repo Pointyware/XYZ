3bdd2da8f0d6d1c52e7607f16e336eb450af2082
Taush Sampley <taushsampley@gmail.com>
2024-07-14 12:16:53 -0500

    Add loading view resources to ui dependencies

--- core/ui/src/commonMain/kotlin/di/UiDependencies.kt
+++ core/ui/src/commonMain/kotlin/di/UiDependencies.kt
@@ -1,12 +1,18 @@
 package org.pointyware.xyz.core.ui.di
 
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import org.koin.core.component.KoinComponent
+import org.koin.core.component.get
+import org.pointyware.xyz.core.ui.ui.LoadingViewResources
 
 /**
  */
 interface UiDependencies {
+    val loadingViewResources: LoadingViewResources
 }
 
 class KoinUiDependencies : UiDependencies, KoinComponent {
-
+    override val loadingViewResources: LoadingViewResources
+        get() = get()
 }
