29601545de5f3dc96e36b02f364b035f61ca6df3
Taush Sampley <taushsampley@gmail.com>
2024-07-14 12:38:28 -0500

    Create authorization view with state hoisted out of composable

---
+++ core/ui/src/commonMain/kotlin/ui/AuthorizationView.kt
@@ -0,0 +1,63 @@
+package org.pointyware.xyz.core.ui.ui
+
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.material3.Button
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+data class AuthorizationViewState(
+    val isLogin: Boolean = true,
+    val email: String = "",
+    val password: String = "",
+    val passwordConfirmation: String = "",
+    val isSubmitEnabled: Boolean = false,
+)
+
+@Composable
+fun AuthorizationView(
+    state: AuthorizationViewState,
+    modifier: Modifier = Modifier,
+    onEmailChange: (String) -> Unit,
+    onPasswordChange: (String) -> Unit,
+    onPasswordConfirmationChange: (String) -> Unit,
+    onSubmit: () -> Unit,
+    onSwitch: () -> Unit,
+) {
+    OutlinedTextField(
+        value = state.email,
+        onValueChange = onEmailChange,
+        label = { Text("Email") },
+        modifier = modifier
+    )
+    OutlinedTextField(
+        value = state.password,
+        onValueChange = onPasswordChange,
+        label = { Text("Password") },
+        modifier = modifier
+    )
+    AnimatedVisibility(
+        visible = state.isLogin.not()
+    ) {
+        OutlinedTextField(
+            value = state.passwordConfirmation,
+            onValueChange = onPasswordConfirmationChange,
+            label = { Text("Password Confirmation") },
+            modifier = modifier
+        )
+    }
+
+    Button(
+        onClick = onSubmit,
+        enabled = state.isSubmitEnabled,
+    ) {
+        Text("Submit")
+    }
+    Button(onClick = onSwitch) {
+        when (state.isLogin) {
+            true -> Text("New Account")
+            false -> Text("Existing Account")
+        }
+    }
+}
