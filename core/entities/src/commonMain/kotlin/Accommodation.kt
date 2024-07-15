db7f0e9684712297a6c8b4ac2ab5094a6610107c
Taush Sampley <taushsampley@gmail.com>
2024-07-15 17:44:56 -0500

    Add supporting types

---
+++ core/entities/src/commonMain/kotlin/Accommodation.kt
@@ -0,0 +1,7 @@
+package org.pointyware.xyz.core.entities
+
+/**
+ * Describes a driver accommodation that can be provided to a rider with a disability.
+ */
+sealed class Accommodation {
+}
---
+++ core/entities/src/commonMain/kotlin/Disability.kt
@@ -0,0 +1,12 @@
+package org.pointyware.xyz.core.entities
+
+/**
+ * Describes a rider disability that may restrict driver acceptance.
+ */
+sealed class Disability {
+    data object Blind : Disability()
+    data object Deaf : Disability()
+    data object Wheelchair : Disability()
+    data object ServiceAnimal : Disability()
+    data object Allergy : Disability()
+}
