0a50096e1e0e2b9d4408ada378b4b512c2f436e0
Taush Sampley <taushsampley@gmail.com>
2024-07-15 11:07:18 -0500

    add extra types for multiplatform definition

--- app-desktop/src/test/kotlin/di/KoinValidationTest.kt
+++ app-desktop/src/test/kotlin/di/KoinValidationTest.kt
@@ -1,5 +1,7 @@
 package org.pointyware.xyz.desktop.di
 
+import io.ktor.client.HttpClientConfig
+import io.ktor.client.engine.HttpClientEngine
 import org.koin.core.annotation.KoinExperimentalAPI
 import org.koin.test.check.checkKoinModules
 import org.koin.test.verify.verify
@@ -12,7 +14,7 @@ class KoinValidationTest {
     @OptIn(KoinExperimentalAPI::class)
     @Test
     fun verifyKoinConfiguration() {
-        topLevelModule.verify()
+        topLevelModule.verify(extraTypes = listOf(HttpClientEngine::class, HttpClientConfig::class))
     }
 
     @Test
