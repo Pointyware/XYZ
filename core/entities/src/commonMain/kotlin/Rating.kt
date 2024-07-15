87816b02c4b14565f1ceac2d2477c8d60570901a
Taush Sampley <taushsampley@gmail.com>
2024-07-15 17:21:45 -0500

    Add supporting entities: rating, review

---
+++ core/entities/src/commonMain/kotlin/Rating.kt
@@ -0,0 +1,12 @@
+package org.pointyware.xyz.core.entities
+
+/**
+ *
+ */
+enum class Rating {
+    ONE,
+    TWO,
+    THREE,
+    FOUR,
+    FIVE
+}
---
+++ core/entities/src/commonMain/kotlin/Review.kt
@@ -0,0 +1,10 @@
+package org.pointyware.xyz.core.entities
+
+/**
+ * A review of a rider or driver.
+ */
+data class Review(
+    val poster: Profile,
+    val rating: Rating,
+    val comment: String
+)
