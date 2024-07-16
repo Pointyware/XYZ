bfd2f9110a59eb89597d668320e2de2978ef2d39
Taush Sampley <taushsampley@gmail.com>
2024-07-16 13:25:20 -0500

    Create CreateUserUseCase separate from LoginUseCase

---
+++ feature/login/src/commonMain/kotlin/interactors/CreateUserUseCase.kt
@@ -0,0 +1,18 @@
+/*
+ * Copyright (c) 2024 Pointyware
+ */
+
+package org.pointyware.xyz.feature.login.interactors
+
+import org.pointyware.xyz.feature.login.data.ProfileRepository
+
+/**
+ * Attempts to create a new user.
+ */
+class CreateUserUseCase(
+    private val profileRepository: ProfileRepository
+) {
+    suspend operator fun invoke(email: String, password: String): Result<Unit> {
+        return profileRepository.createUser(email, password)
+    }
+}
--- feature/login/src/commonMain/kotlin/interactors/LoginUseCase.kt
+++ feature/login/src/commonMain/kotlin/interactors/LoginUseCase.kt
@@ -5,14 +5,16 @@
 package org.pointyware.xyz.feature.login.interactors
 
 import org.pointyware.xyz.core.entities.Profile
+import org.pointyware.xyz.feature.login.data.Authorization
 import org.pointyware.xyz.feature.login.data.ProfileRepository
 
 /**
+ * Attempts to log in a user.
  */
 class LoginUseCase(
     private val profileRepository: ProfileRepository
 ) {
-    suspend operator fun invoke(email: String, password: String): Result<Profile> {
+    suspend operator fun invoke(email: String, password: String): Result<Authorization> {
         return profileRepository.login(email, password)
     }
 }
