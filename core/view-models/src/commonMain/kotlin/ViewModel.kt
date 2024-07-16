618dcf4de9b7a4633ddd5ac70b533103ced60902
Taush Sampley <taushsampley@gmail.com>
2024-07-16 12:58:05 -0500

    rename BaseViewModel->ViewModel
    * fix viewModelScope

--- core/view-models/src/commonMain/kotlin/ViewModel.kt
+++ core/view-models/src/commonMain/kotlin/ViewModel.kt
@@ -7,17 +7,21 @@ package org.pointyware.xyz.core.viewmodels
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.SupervisorJob
+import kotlinx.coroutines.cancel
 
 /**
  * Defines base behavior for all view models.
  */
-interface BaseViewModel {
+abstract class ViewModel {
 
     /**
      * The scope for the view model. The default uses the main dispatcher and a supervisor job.
      */
-    val viewModelScope: CoroutineScope
-        get() {
-            return CoroutineScope(Dispatchers.Main + SupervisorJob())
-        }
+    protected val viewModelScope: CoroutineScope by lazy {
+        CoroutineScope(Dispatchers.Main + SupervisorJob())
+    }
+
+    fun dispose() {
+        viewModelScope.coroutineContext.cancel()
+    }
 }
