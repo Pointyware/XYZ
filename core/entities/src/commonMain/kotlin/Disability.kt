d28269dab307365e403b5179644c241591d47439
Taush Sampley <taushsampley@gmail.com>
2024-07-16 11:02:48 -0500

    Add disability references

--- core/entities/src/commonMain/kotlin/Disability.kt
+++ core/entities/src/commonMain/kotlin/Disability.kt
@@ -6,11 +6,15 @@ package org.pointyware.xyz.core.entities
 
 /**
  * Describes a rider disability that may restrict driver acceptance.
+ *
+ * References:
+ * 1. https://aafa.org/asthma/living-with-asthma/asthma-allergies-and-the-american-with-disabilities-act/
+ * 2. https://www.ada.gov/regs2010/service_animal_qa.html
  */
 sealed class Disability {
     data object Blind : Disability()
     data object Deaf : Disability()
-    data object Wheelchair : Disability()
+    data object Mobility : Disability()
     data object ServiceAnimal : Disability()
     data object Allergy : Disability()
 }
