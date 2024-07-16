c0ba157c98d24000dc202717c8f9591f407e6d63
Taush Sampley <taushsampley@gmail.com>
2024-07-16 08:04:15 -0500

    Add supporting entity LatLong

---
+++ core/entities/src/commonMain/kotlin/LatLong.kt
@@ -0,0 +1,13 @@
+/*
+ * Copyright (c) 2024 Pointyware
+ */
+
+package org.pointyware.xyz.core.entities
+
+/**
+ * A simple data class to represent a geographic location in latitude and longitude.
+ */
+data class LatLong(
+    val latitude: Double,
+    val longitude: Double
+)
