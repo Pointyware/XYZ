7201fdebeb8c8026c0fc2f875216fa7465d330a8
Taush Sampley <taushsampley@gmail.com>
2024-07-17 11:58:09 -0500

    copy jvm client setup for native client

--- core/remote/src/nativeMain/kotlin/KtorClient.native.kt
+++ core/remote/src/nativeMain/kotlin/KtorClient.native.kt
@@ -6,12 +6,24 @@ package org.pointyware.xyz.core.remote
 
 import io.ktor.client.HttpClient
 import io.ktor.client.engine.cio.CIO
+import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.client.plugins.defaultRequest
+import io.ktor.client.plugins.resources.Resources
+import io.ktor.http.URLProtocol
 
 /**
  *
  */
 actual fun getClient(): HttpClient {
     return HttpClient(CIO) {
+        install(Resources)
+        install(ContentNegotiation)
+        defaultRequest {
+            host = "api.xyz.pointyware.org"
+            url { protocol = URLProtocol.HTTPS }
+        }
+        engine {
 
+        }
     }
 }
