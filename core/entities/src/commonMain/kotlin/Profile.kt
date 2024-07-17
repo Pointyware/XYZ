e7dcd176b0b471f3b577eead20280cbb66df3037
Taush Sampley <taushsampley@gmail.com>
2024-07-17 09:53:56 -0500

    Create driver/rider profiles

--- core/entities/src/commonMain/kotlin/Profile.kt
+++ core/entities/src/commonMain/kotlin/Profile.kt
@@ -7,11 +7,30 @@ package org.pointyware.xyz.core.entities
 /**
  *
  */
-class Profile(
-    val email: String,
-    val name: Name,
-    val phone: PhoneNumber,
-    val gender: Gender,
-    val picture: Uri,
-) {
+interface Profile {
+    val email: String
+    val name: Name
+    val phone: PhoneNumber
+    val gender: Gender
+    val picture: Uri
 }
+
+class DriverProfile(
+    override val email: String,
+    override val name: Name,
+    override val phone: PhoneNumber,
+    override val gender: Gender,
+    override val picture: Uri,
+    val accommodations: Set<Accommodation>,
+    val company: Company
+): Profile
+
+class RiderProfile(
+    override val email: String,
+    override val name: Name,
+    override val phone: PhoneNumber,
+    override val gender: Gender,
+    override val picture: Uri,
+    val disabilities: Set<Disability>,
+    val preferences: String
+): Profile
