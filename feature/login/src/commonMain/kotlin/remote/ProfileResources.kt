98c65b89cbd0b2b96c39f013666d6dc5e5b58c76
Taush Sampley <taushsampley@gmail.com>
2024-07-17 09:59:06 -0500

    Implement profile service

---
+++ feature/login/src/commonMain/kotlin/remote/ProfileResources.kt
@@ -0,0 +1,16 @@
+/*
+ * Copyright (c) 2024 Pointyware. Use of this software is governed by the GPL-3.0 license.
+ */
+
+package org.pointyware.xyz.feature.login.remote
+
+import io.ktor.resources.Resource
+
+/**
+ *
+ */
+@Resource("profile")
+sealed interface Profile {
+    @Resource("{userId}")
+    data class Id(val userId: String)
+}
--- feature/login/src/commonMain/kotlin/remote/ProfileService.kt
+++ feature/login/src/commonMain/kotlin/remote/ProfileService.kt
@@ -5,15 +5,52 @@
 package org.pointyware.xyz.feature.login.remote
 
 import io.ktor.client.HttpClient
-import org.pointyware.xyz.feature.login.data.Authorization
+import io.ktor.client.call.body
+import io.ktor.client.plugins.resources.delete
+import io.ktor.client.plugins.resources.get
+import io.ktor.client.plugins.resources.patch
+import io.ktor.client.request.setBody
+import org.pointyware.xyz.core.entities.Uuid
+import org.pointyware.xyz.core.entities.Profile as ProfileEntity
 
 /**
  * Exposes profile-related actions to be performed by a remote service.
  */
 interface ProfileService {
+    suspend fun getProfile(userId: Uuid): Result<ProfileEntity>
+    suspend fun updateProfile(userId: Uuid, profile: ProfileEntity): Result<ProfileEntity>
+    suspend fun deleteProfile(userId: Uuid): Result<Unit>
 }
 
 class KtorProfileService(
     val client: HttpClient
 ): ProfileService {
+    override suspend fun getProfile(userId: Uuid): Result<ProfileEntity> {
+        try {
+            val response = client.get(Profile.Id(userId.toString()))
+            return Result.success(response.body<ProfileEntity>())
+        } catch (e: Exception) {
+            return Result.failure(e)
+        }
+    }
+
+    override suspend fun updateProfile(userId: Uuid, profile: ProfileEntity): Result<ProfileEntity> {
+        try {
+            val response = client.patch(Profile.Id(userId.toString())) {
+                setBody(profile)
+            }
+            return Result.success(response.body<ProfileEntity>())
+        } catch (e: Exception) {
+            return Result.failure(e)
+        }
+    }
+
+    override suspend fun deleteProfile(userId: Uuid): Result<Unit> {
+        try {
+            client.delete(Profile.Id(userId.toString()))
+            return Result.success(Unit)
+        } catch (e: Exception) {
+            return Result.failure(e)
+        }
+    }
 }
