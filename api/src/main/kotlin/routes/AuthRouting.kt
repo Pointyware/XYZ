88d8f2758360d38f57bab5cd1d227955b4450527
Taush Sampley <taushsampley@gmail.com>
2024-07-17 08:12:54 -0500

    replace auth placeholder with login/create endpoints

--- api/src/main/kotlin/routes/AuthRouting.kt
+++ api/src/main/kotlin/routes/AuthRouting.kt
@@ -8,12 +8,16 @@ import io.ktor.server.application.call
 import io.ktor.server.response.respondText
 import io.ktor.server.routing.Routing
 import io.ktor.server.routing.get
+import io.ktor.server.routing.post
 
 /**
  * Directs auth requests to the appropriate controller.
  */
 fun Routing.auth() {
-    get("/auth") {
-        call.respondText("Hello World!")
+    post("/auth/login") {
+        TODO("access sql database to authenticate user")
+    }
+    post("/auth/create") {
+        TODO("access sql database to create user")
     }
 }
