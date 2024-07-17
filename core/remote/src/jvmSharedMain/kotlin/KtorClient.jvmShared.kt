65c2b7f6e63ebdf0f743d68b536a9df95b630ea6
Taush Sampley <taushsampley@gmail.com>
2024-07-17 08:14:38 -0500

    setup client defaults

--- core/remote/src/jvmSharedMain/kotlin/KtorClient.jvmShared.kt
+++ core/remote/src/jvmSharedMain/kotlin/KtorClient.jvmShared.kt
@@ -6,12 +6,26 @@ package org.pointyware.xyz.core.remote
 
 import io.ktor.client.HttpClient
 import io.ktor.client.engine.okhttp.OkHttp
+import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.client.plugins.defaultRequest
+import io.ktor.client.plugins.resources.Resources
+import io.ktor.http.URLProtocol
 
 /**
  *
  */
 actual fun getClient(): HttpClient {
     return HttpClient(OkHttp) {
+        install(Resources)
+        install(ContentNegotiation)
+        defaultRequest {
+            host = "api.xyz.pointyware.org"
+            url { protocol = URLProtocol.HTTPS }
+        }
+        engine {
+            config {
 
+            }
+        }
     }
 }
