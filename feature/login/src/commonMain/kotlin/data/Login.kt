66d32a8b77155d8eedf4443c6b8d88b0d3025edd
Taush Sampley <taushsampley@gmail.com>
2024-07-17 09:54:43 -0500

    Create data type to group authorization with returned profile

---
+++ feature/login/src/commonMain/kotlin/data/Login.kt
@@ -0,0 +1,16 @@
+/*
+ * Copyright (c) 2024 Pointyware. Use of this software is governed by the GPL-3.0 license.
+ */
+
+package org.pointyware.xyz.feature.login.data
+
+import org.pointyware.xyz.core.entities.Profile
+import org.pointyware.xyz.feature.login.data.Authorization
+
+/**
+ * Combines the general authorization data with the user profile.
+ */
+data class Login(
+    val authorization: Authorization,
+    val profile: Profile
+)
