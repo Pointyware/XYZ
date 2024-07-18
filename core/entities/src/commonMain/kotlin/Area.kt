643f1030e7ab995966ce37b4eb8a5261bde01217
Taush Sampley <taushsampley@gmail.com>
2024-07-18 12:26:28 -0500

    Add Area type

---
+++ core/entities/src/commonMain/kotlin/Area.kt
@@ -0,0 +1,14 @@
+/*
+ * Copyright (c) 2024 Pointyware. Use of this software is governed by the GPL-3.0 license.
+ */
+
+package org.pointyware.xyz.core.entities
+
+/**
+ * Describes a geographical area.
+ */
+sealed interface Area {
+    data class Circular(val center: LatLong, val radius: Length): Area
+    data class Rectangular(val topLeft: LatLong, val bottomRight: LatLong): Area
+    data class Polygonal(val vertices: List<LatLong>): Area
+}
