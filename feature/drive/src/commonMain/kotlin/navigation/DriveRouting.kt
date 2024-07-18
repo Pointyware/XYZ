e9c43fd5c798e16562fd71f2f89fc32fb8c2e8d2
Taush Sampley <taushsampley@gmail.com>
2024-07-17 21:38:30 -0500

    add ride/drive routing

---
+++ feature/drive/src/commonMain/kotlin/navigation/DriveRouting.kt
@@ -0,0 +1,40 @@
+/*
+ * Copyright (c) 2024 Pointyware. Use of this software is governed by the GPL-3.0 license.
+ */
+
+package org.pointyware.xyz.drive.navigation
+
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import org.koin.mp.KoinPlatform
+import org.pointyware.xyz.core.navigation.StaticRoute
+import org.pointyware.xyz.core.navigation.XyzRootScope
+import org.pointyware.xyz.core.navigation.di.NavigationDependencies
+import org.pointyware.xyz.drive.di.DriveDependencies
+import org.pointyware.xyz.drive.ui.DriveScreen
+
+val driveRoute = StaticRoute("drive", Unit)
+
+/**
+ *
+ */
+@Composable
+fun XyzRootScope.driveRouting() {
+
+    val di = remember { KoinPlatform.getKoin() }
+    val driveDependencies = remember { di.get<DriveDependencies>() }
+    val navigationDependencies = remember { di.get<NavigationDependencies>() }
+
+    location(driveRoute) {
+        val driveViewModel = remember { driveDependencies.getDriveViewModel() }
+        val navController = remember { navigationDependencies.getNavController() }
+
+        DriveScreen(
+            viewModel = driveViewModel,
+            navController = navController,
+            modifier = Modifier.fillMaxSize()
+        )
+    }
+}
---
+++ feature/ride/src/commonMain/kotlin/navigation/RideRouting.kt
@@ -0,0 +1,39 @@
+/*
+ * Copyright (c) 2024 Pointyware. Use of this software is governed by the GPL-3.0 license.
+ */
+
+package org.pointyware.xyz.feature.ride.navigation
+
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import org.koin.mp.KoinPlatform
+import org.pointyware.xyz.core.navigation.StaticRoute
+import org.pointyware.xyz.core.navigation.XyzRootScope
+import org.pointyware.xyz.core.navigation.di.NavigationDependencies
+import org.pointyware.xyz.feature.ride.di.RideDependencies
+import org.pointyware.xyz.feature.ride.ui.RideScreen
+
+val rideRoute = StaticRoute("ride", Unit)
+
+/**
+ *
+ */
+@Composable
+fun XyzRootScope.rideRouting() {
+    val di = remember { KoinPlatform.getKoin() }
+    val rideDependencies = remember { di.get<RideDependencies>() }
+    val navigationDependencies = remember { di.get<NavigationDependencies>() }
+
+    location(rideRoute) {
+        val rideViewModel = remember { rideDependencies.getRideViewModel() }
+        val navController = remember { navigationDependencies.getNavController() }
+
+        RideScreen(
+            viewModel = rideViewModel,
+            navController = navController,
+            modifier = Modifier.fillMaxSize()
+        )
+    }
+}
