d0f15c21c03e0986f847dfde26e1511f1502d075
Taush Sampley <taushsampley@gmail.com>
2024-07-17 21:38:45 -0500

    separate profile from login routing

--- feature/login/src/commonMain/kotlin/navigation/ProfileRouting.kt
+++ feature/login/src/commonMain/kotlin/navigation/ProfileRouting.kt
@@ -2,38 +2,47 @@
  * Copyright (c) 2024 Pointyware. Use of this software is governed by the GPL-3.0 license.
  */
 
-package navigation
+package org.pointyware.xyz.feature.login.navigation
 
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import org.koin.mp.KoinPlatform.getKoin
+import org.pointyware.xyz.core.entities.Uuid
 import org.pointyware.xyz.core.navigation.StaticRoute
 import org.pointyware.xyz.core.navigation.XyzRootScope
 import org.pointyware.xyz.core.navigation.di.NavigationDependencies
 import org.pointyware.xyz.feature.login.di.LoginDependencies
-import org.pointyware.xyz.feature.login.ui.AuthorizationScreen
+import org.pointyware.xyz.feature.login.ui.AccountCreationScreen
 
-val loginRoute = StaticRoute("login", Unit)
+val profileRoute = StaticRoute("profile", Unit)
+val accountCreationRoute = profileRoute.fixed("create")
+val userProfileRoute = profileRoute.variable<Uuid>("userId")
 
 /**
  * Sets up all routes for home navigation.
  */
 @Composable
-fun XyzRootScope.loginRouting() {
+fun XyzRootScope.profileRouting() {
     val di = remember { getKoin() }
     val loginDependencies = remember { di.get<LoginDependencies>() }
     val navigationDependencies = remember { di.get<NavigationDependencies>() }
 
-    location(loginRoute) {
+    location(accountCreationRoute) {
         val authorizationViewModel = remember { loginDependencies.getAuthorizationViewModel() }
         val navController = remember { navigationDependencies.getNavController() }
 
-        AuthorizationScreen(
-            authorizationViewModel = authorizationViewModel,
-            navController = navController,
+        AccountCreationScreen(
             modifier = Modifier.fillMaxSize()
         )
+    }
+    location(userProfileRoute) {
+        val profileViewModel = remember { TODO("Implement profile view model") }
+        val navController = remember { navigationDependencies.getNavController() }
+
+//        UserProfileScreen(
+//
+//        )
     }
 }
