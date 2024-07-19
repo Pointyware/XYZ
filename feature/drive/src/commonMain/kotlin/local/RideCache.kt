7114855dbddc400a3b7565ba43d9e54e465af20d
Taush Sampley <taushsampley@gmail.com>
2024-07-19 08:08:18 -0500

    flesh out RideCache

--- feature/drive/src/commonMain/kotlin/local/RideCache.kt
+++ feature/drive/src/commonMain/kotlin/local/RideCache.kt
@@ -4,30 +4,46 @@
 
 package org.pointyware.xyz.drive.local
 
+import kotlinx.coroutines.flow.Flow
+import org.pointyware.xyz.core.entities.Ride
 import org.pointyware.xyz.drive.data.RideSearchResult
 
 /**
  *
  */
 interface RideCache {
-    fun saveDestinations(query: String, searchResult: RideSearchResult)
-    fun getDestinations(query: String): RideSearchResult?
-    fun dropDestinations(query: String)
+    suspend fun saveDestinations(query: String, searchResult: RideSearchResult)
+    suspend fun getDestinations(query: String): RideSearchResult?
+    suspend fun dropDestinations(query: String)
+    suspend fun saveRide(ride: Ride)
+    suspend fun dropRide(ride: Ride)
+    suspend fun createRideFilter(criteria: Ride.Criteria): Result<Flow<Ride>>
 }
 
 class RideCacheImpl(
     private val rideDao: RideDao
 ): RideCache {
-
-    override fun saveDestinations(query: String, searchResult: RideSearchResult) {
+    override suspend fun saveDestinations(query: String, searchResult: RideSearchResult) {
         TODO("Not yet implemented")
     }
 
-    override fun getDestinations(query: String): RideSearchResult? {
+    override suspend fun getDestinations(query: String): RideSearchResult? {
         TODO("Not yet implemented")
     }
 
-    override fun dropDestinations(query: String) {
+    override suspend fun dropDestinations(query: String) {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun saveRide(ride: Ride) {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun dropRide(ride: Ride) {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun createRideFilter(criteria: Ride.Criteria): Result<Flow<Ride>> {
         TODO("Not yet implemented")
     }
 }
