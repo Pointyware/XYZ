f90e69148e743050962d06d4bbd26c929838d5b2
Taush Sampley <taushsampley@gmail.com>
2024-07-19 08:08:08 -0500

    flesh out RideService

--- feature/drive/src/commonMain/kotlin/remote/RideService.kt
+++ feature/drive/src/commonMain/kotlin/remote/RideService.kt
@@ -4,11 +4,36 @@
 
 package org.pointyware.xyz.drive.remote
 
+import kotlinx.coroutines.flow.Flow
+import org.pointyware.xyz.core.entities.Ride
 import org.pointyware.xyz.drive.data.RideSearchResult
 
 /**
- *
+ * Defines the actions supported by the remote service.
  */
 interface RideService {
-    fun searchDestinations(query: String): Result<RideSearchResult>
+    /**
+     * Searches for destinations that match the given query.
+     */
+    suspend fun searchDestinations(query: String): Result<RideSearchResult>
+
+    /**
+     * Posts a ride to the service.
+     */
+    suspend fun postRide(ride: Ride): Result<Ride>
+
+    /**
+     * Cancels a ride.
+     */
+    suspend fun cancelRide(ride: Ride): Result<Ride>
+
+    /**
+     * Completes a ride.
+     */
+    suspend fun completeRide(ride: Ride): Result<Ride>
+
+    /**
+     * Creates a filter for rides that match the given criteria.
+     */
+    suspend fun createRideFilter(criteria: Ride.Criteria): Result<Flow<Ride>>
 }
