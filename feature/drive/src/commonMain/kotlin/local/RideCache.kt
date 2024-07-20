a67d2d12976d77ee3b56dcfff1db83a3137f38b5
Taush Sampley <taushsampley@gmail.com>
2024-07-20 13:58:43 -0500

    naively implement cache

--- feature/drive/src/commonMain/kotlin/local/RideCache.kt
+++ feature/drive/src/commonMain/kotlin/local/RideCache.kt
@@ -6,6 +6,7 @@ package org.pointyware.xyz.drive.local
 
 import kotlinx.coroutines.flow.Flow
 import org.pointyware.xyz.core.entities.Ride
+import org.pointyware.xyz.core.entities.Uuid
 import org.pointyware.xyz.drive.data.RideSearchResult
 
 /**
@@ -17,33 +18,33 @@ interface RideCache {
     suspend fun dropDestinations(query: String)
     suspend fun saveRide(ride: Ride)
     suspend fun dropRide(ride: Ride)
-    suspend fun createRideFilter(criteria: Ride.Criteria): Result<Flow<Ride>>
 }
 
 class RideCacheImpl(
-    private val rideDao: RideDao
+
 ): RideCache {
+
+    private val queryCache = mutableMapOf<String, RideSearchResult>()
+
     override suspend fun saveDestinations(query: String, searchResult: RideSearchResult) {
-        TODO("Not yet implemented")
+        queryCache[query] = searchResult
     }
 
     override suspend fun getDestinations(query: String): RideSearchResult? {
-        TODO("Not yet implemented")
+        return queryCache[query]
     }
 
     override suspend fun dropDestinations(query: String) {
-        TODO("Not yet implemented")
+        queryCache.remove(query)
     }
 
+    private val rideCache = mutableMapOf<Uuid, Ride>()
+
     override suspend fun saveRide(ride: Ride) {
-        TODO("Not yet implemented")
+        rideCache[ride.id] = ride
     }
 
     override suspend fun dropRide(ride: Ride) {
-        TODO("Not yet implemented")
-    }
-
-    override suspend fun createRideFilter(criteria: Ride.Criteria): Result<Flow<Ride>> {
-        TODO("Not yet implemented")
+        rideCache.remove(ride.id)
     }
 }
