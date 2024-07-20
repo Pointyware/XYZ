4405116f2ab07b6ffcd32381d1ea899d8dfb7ff7
Taush Sampley <taushsampley@gmail.com>
2024-07-19 20:01:46 -0500

    add padding and clip

--- core/ui/src/commonMain/kotlin/components/ErrorDialog.kt
+++ core/ui/src/commonMain/kotlin/components/ErrorDialog.kt
@@ -5,9 +5,16 @@
 package org.pointyware.xyz.core.ui.components
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Dialog
 
 data class ErrorState(
@@ -26,14 +33,22 @@ fun ErrorDialog(
     Dialog(
         onDismissRequest = onDismiss
     ) {
-        Column {
-            Text(
-                text = state.message
-            )
-            Button(
-                onClick = onDismiss
+        Surface(
+            modifier = Modifier
+                .clip(MaterialTheme.shapes.medium)
+        ) {
+            Column(
+                modifier = Modifier.padding(16.dp),
+                horizontalAlignment = Alignment.End
             ) {
-                Text(state.dismissLabel)
+                Text(
+                    text = state.message
+                )
+                Button(
+                    onClick = onDismiss
+                ) {
+                    Text(state.dismissLabel)
+                }
             }
         }
     }
