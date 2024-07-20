67ec4ce8230a82ba8e19861d4c4b789655877c46
Taush Sampley <taushsampley@gmail.com>
2024-07-19 20:08:32 -0500

    replace production service with test service

--- feature/login/src/commonMain/kotlin/di/LoginModule.kt
+++ feature/login/src/commonMain/kotlin/di/LoginModule.kt
@@ -4,11 +4,11 @@
 
 package org.pointyware.xyz.feature.login.di
 
-import io.ktor.client.HttpClient
 import navigation.loginRoute
 import org.koin.core.qualifier.named
 import org.koin.dsl.module
 import org.pointyware.xyz.core.data.di.dataQualifier
+import org.pointyware.xyz.core.entities.Uuid
 import org.pointyware.xyz.core.remote.getClient
 import org.pointyware.xyz.feature.login.data.ProfileRepository
 import org.pointyware.xyz.feature.login.data.ProfileRepositoryImpl
@@ -21,7 +21,7 @@ import org.pointyware.xyz.feature.login.local.ProfileCacheImpl
 import org.pointyware.xyz.feature.login.remote.AuthService
 import org.pointyware.xyz.feature.login.remote.KtorProfileService
 import org.pointyware.xyz.feature.login.remote.ProfileService
-import org.pointyware.xyz.feature.login.remote.SimpleAuthService
+import org.pointyware.xyz.feature.login.remote.TestAuthService
 import org.pointyware.xyz.feature.login.viewmodels.AuthorizationViewModel
 import org.pointyware.xyz.feature.login.viewmodels.AuthorizationViewModelImpl
 
@@ -47,7 +47,10 @@ fun featureLoginDataModule() = module {
         get<AuthCache>(), get<AuthService>(), get<ProfileCache>(), get<ProfileService>(),
         get(dataQualifier)
     ) }
-    single<AuthService> { SimpleAuthService(get<HttpClient>()) }
+//    single<AuthService> { SimpleAuthService(get<HttpClient>()) }
+    single<AuthService> { TestAuthService(
+        users = mutableMapOf("foo@bar.com" to TestAuthService.UserEntry("password", Uuid.v4()))
+    )}
     single<AuthCache> { AuthCacheImpl() }
     single<ProfileCache> { ProfileCacheImpl() }
     single<ProfileService> { KtorProfileService(getClient()) }
