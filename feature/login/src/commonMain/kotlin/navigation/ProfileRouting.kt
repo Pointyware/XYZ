85edfc4ac17b1a11e0e3fbf24ea2ab07ea8924cf
Taush Sampley <taushsampley@gmail.com>
2024-07-20 22:26:29 -0500

    move account creation screens to profile routing

--- feature/login/src/commonMain/kotlin/navigation/ProfileRouting.kt
+++ feature/login/src/commonMain/kotlin/navigation/ProfileRouting.kt
@@ -8,19 +8,22 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
-import org.koin.mp.KoinPlatform.getKoin
 import org.pointyware.xyz.core.entities.Uuid
 import org.pointyware.xyz.core.navigation.StaticRoute
 import org.pointyware.xyz.core.navigation.XyzRootScope
 import org.pointyware.xyz.core.navigation.di.NavigationDependencies
-import org.pointyware.xyz.feature.login.di.LoginDependencies
+import org.pointyware.xyz.drive.ui.DriverProfileCreationView
 import org.pointyware.xyz.feature.login.di.ProfileDependencies
-import org.pointyware.xyz.feature.login.ui.AccountCreationScreen
+import org.pointyware.xyz.feature.login.ui.RoleSelectionView
 import org.pointyware.xyz.feature.login.ui.UserProfileScreen
+import org.pointyware.xyz.feature.ride.ui.RiderProfileCreationView
 
 val profileRoute = StaticRoute("profile", Unit)
 val accountCreationRoute = profileRoute.fixed("create")
-val userProfileRoute = profileRoute.variable<Uuid>("userId")
+val roleSelectionRoute = accountCreationRoute.fixed("role")
+val driverCreationRoute = accountCreationRoute.fixed("driver")
+val riderCreationRoute = accountCreationRoute.fixed("rider")
+val userProfileRoute = profileRoute.variable<Uuid>("user-{userId}")
 
 /**
  * Sets up all routes for home navigation.
@@ -31,13 +34,33 @@ fun XyzRootScope.profileRouting(
     navigationDependencies: NavigationDependencies
 ) {
 
-    location(accountCreationRoute) {
-        val accountCreationViewModel = remember { profileDependencies.getAccountCreationViewModel() }
-        val navController = remember { navigationDependencies.getNavController() }
+    companyRouting(
+        navigationDependencies.getNavController()
+    )
 
-        AccountCreationScreen(
-            viewModel = accountCreationViewModel,
-            navController = navController,
+    location(roleSelectionRoute) {
+        RoleSelectionView(
+            modifier = Modifier.fillMaxSize(),
+            onConfirm = TODO()
+        )
+    }
+    location(driverCreationRoute) {
+        DriverProfileCreationView(
+            state = TODO(),
+            modifier = Modifier.fillMaxSize(),
+            onProfileImageSelected = TODO(),
+            onGivenNameChange = TODO(),
+            onMiddleNameChange = TODO(),
+            onFamilyNameChange = TODO(),
+            onBirthdateChange = TODO(),
+            onGenderSelected = TODO(),
+            onCompanySelected = TODO(),
+            onSubmit = TODO(),
+        )
+    }
+    location(riderCreationRoute) {
+        RiderProfileCreationView(
+
         )
     }
     location(userProfileRoute) {
