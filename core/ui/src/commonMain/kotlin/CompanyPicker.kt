aed0150f4360f9b2dd2c9bb7ce26121859d59533
Taush Sampley <taushsampley@gmail.com>
2024-07-21 00:05:14 -0500

    Start company picker

---
+++ core/ui/src/commonMain/kotlin/CompanyPicker.kt
@@ -0,0 +1,21 @@
+/*
+ * Copyright (c) 2024 Pointyware. Use of this software is governed by the GPL-3.0 license.
+ */
+
+package org.pointyware.xyz.core.ui
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import org.pointyware.xyz.core.entities.Uuid
+
+/**
+ * A search bar that allows the user to search for a company that shows up in a list or register a new company.
+ */
+@Composable
+fun CompanyPicker(
+    modifier: Modifier = Modifier,
+    onCompanySelected: (Uuid) -> Unit
+) {
+    TODO("Not yet Implemented" +
+            "")
+}
