c65672da9e3e375242af0a7e237d0a9adba0c303
Taush Sampley <taushsampley@gmail.com>
2024-07-21 08:13:15 -0500

    provide base view models to driver/rider view models

--- feature/login/src/commonMain/kotlin/di/ProfileModule.kt
+++ feature/login/src/commonMain/kotlin/di/ProfileModule.kt
@@ -4,19 +4,29 @@
 
 package org.pointyware.xyz.feature.login.di
 
+import org.koin.core.qualifier.named
+import org.koin.core.qualifier.qualifier
 import org.koin.dsl.module
+import org.pointyware.xyz.core.viewmodels.DriverProfileUiState
+import org.pointyware.xyz.core.viewmodels.RiderProfileUiState
 import org.pointyware.xyz.feature.login.viewmodels.DriverProfileCreationViewModel
 import org.pointyware.xyz.feature.login.viewmodels.DriverProfileCreationViewModelImpl
+import org.pointyware.xyz.feature.login.viewmodels.ProfileCreationViewModel
 import org.pointyware.xyz.feature.login.viewmodels.RiderProfileCreationViewModel
 import org.pointyware.xyz.feature.login.viewmodels.RiderProfileCreationViewModelImpl
+
+val driverQualifier = named("driver")
+val riderQualifier = named("rider")
 
 fun featureProfileModule() = module {
     single<ProfileDependencies> { KoinProfileDependencies() }
 
     factory<DriverProfileCreationViewModel> {
-        DriverProfileCreationViewModelImpl()
+        DriverProfileCreationViewModelImpl(get<ProfileCreationViewModel<DriverProfileUiState>>(qualifier = driverQualifier))
     }
     factory<RiderProfileCreationViewModel> {
-        RiderProfileCreationViewModelImpl()
+        RiderProfileCreationViewModelImpl(get<ProfileCreationViewModel<RiderProfileUiState>>(qualifier = riderQualifier))
     }
+
+
 }
