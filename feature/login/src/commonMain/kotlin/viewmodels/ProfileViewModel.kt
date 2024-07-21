a40ad7e46af7c9f13c4cc5fa5f3ac8d6a50a27f3
Taush Sampley <taushsampley@gmail.com>
2024-07-21 08:03:06 -0500

    make profile view model an interface with separate implementation

--- feature/login/src/commonMain/kotlin/viewmodels/ProfileViewModel.kt
+++ feature/login/src/commonMain/kotlin/viewmodels/ProfileViewModel.kt
@@ -4,11 +4,25 @@
 
 package org.pointyware.xyz.feature.login.viewmodels
 
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import org.pointyware.xyz.core.viewmodels.EmptyProfileUiState
 import org.pointyware.xyz.core.viewmodels.KoinViewModel
+import org.pointyware.xyz.core.viewmodels.ProfileUiState
+import org.pointyware.xyz.feature.login.data.ProfileRepository
+
+interface ProfileViewModel {
+    val state: StateFlow<ProfileUiState>
+}
 
 /**
  *
  */
-class ProfileViewModel(): KoinViewModel() {
+abstract class ProfileViewModelImpl(
+    private val profileRepository: ProfileRepository
+): KoinViewModel(), ProfileViewModel {
 
+    private val mutableState = MutableStateFlow<ProfileUiState>(EmptyProfileUiState)
+    override val state: StateFlow<ProfileUiState>
+        get() = mutableState
 }
