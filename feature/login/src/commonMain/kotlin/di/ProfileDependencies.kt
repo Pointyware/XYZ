bab48a110b628d1613ffe563d5c539db1f692d1f
Taush Sampley <taushsampley@gmail.com>
2024-07-21 07:37:56 -0500

    Add rider/driver profile creation view model

--- feature/login/src/commonMain/kotlin/di/ProfileDependencies.kt
+++ feature/login/src/commonMain/kotlin/di/ProfileDependencies.kt
@@ -6,18 +6,31 @@ package org.pointyware.xyz.feature.login.di
 
 import org.koin.core.component.KoinComponent
 import org.koin.core.component.get
+import org.pointyware.xyz.feature.login.viewmodels.DriverProfileCreationViewModel
 import org.pointyware.xyz.feature.login.viewmodels.ProfileViewModel
+import org.pointyware.xyz.feature.login.viewmodels.RiderProfileCreationViewModel
 
 /**
  */
 interface ProfileDependencies {
     fun getProfileViewModel(): ProfileViewModel
+    fun getRiderProfileCreationViewModel(): RiderProfileCreationViewModel
+    fun getDriverProfileCreationViewModel(): DriverProfileCreationViewModel
 }
 
-class KoinProfileDependencies: ProfileDependencies, KoinComponent {
+class KoinProfileDependencies(
+
+): ProfileDependencies, KoinComponent {
 
     override fun getProfileViewModel(): ProfileViewModel {
         return get()
     }
 
+    override fun getRiderProfileCreationViewModel(): RiderProfileCreationViewModel {
+        return get()
+    }
+
+    override fun getDriverProfileCreationViewModel(): DriverProfileCreationViewModel {
+        return get()
+    }
 }
