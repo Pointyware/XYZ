837e4b9db5e89039bdff381a54ee3dba1c0ddcab
Taush Sampley <taushsampley@gmail.com>
2024-07-21 17:22:01 -0500

    remove unnecessary factories

--- feature/login/src/commonMain/kotlin/di/ProfileModule.kt
+++ feature/login/src/commonMain/kotlin/di/ProfileModule.kt
@@ -4,34 +4,19 @@
 
 package org.pointyware.xyz.feature.login.di
 
-import org.koin.core.qualifier.named
 import org.koin.dsl.module
-import org.pointyware.xyz.core.viewmodels.DriverProfileUiState
-import org.pointyware.xyz.core.viewmodels.RiderProfileUiState
 import org.pointyware.xyz.feature.login.viewmodels.DriverProfileCreationViewModel
 import org.pointyware.xyz.feature.login.viewmodels.DriverProfileCreationViewModelImpl
-import org.pointyware.xyz.feature.login.viewmodels.ProfileCreationViewModel
-import org.pointyware.xyz.feature.login.viewmodels.ProfileCreationViewModelImpl
 import org.pointyware.xyz.feature.login.viewmodels.RiderProfileCreationViewModel
 import org.pointyware.xyz.feature.login.viewmodels.RiderProfileCreationViewModelImpl
-
-val driverQualifier = named("driver")
-val riderQualifier = named("rider")
 
 fun featureProfileModule() = module {
     single<ProfileDependencies> { KoinProfileDependencies() }
 
     factory<DriverProfileCreationViewModel> {
-        DriverProfileCreationViewModelImpl(get<ProfileCreationViewModel<DriverProfileUiState>>(qualifier = driverQualifier))
+        DriverProfileCreationViewModelImpl()
     }
     factory<RiderProfileCreationViewModel> {
-        RiderProfileCreationViewModelImpl(get<ProfileCreationViewModel<RiderProfileUiState>>(qualifier = riderQualifier))
-    }
-
-    factory<ProfileCreationViewModel<DriverProfileUiState>>(qualifier = driverQualifier) {
-        ProfileCreationViewModelImpl()
-    }
-    factory<ProfileCreationViewModel<RiderProfileUiState>>(qualifier = riderQualifier) {
-        ProfileCreationViewModelImpl()
+        RiderProfileCreationViewModelImpl()
     }
 }
