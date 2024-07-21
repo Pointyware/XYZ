7f5f8ca14a860477cbb646bca6bd945f7f991631
Taush Sampley <taushsampley@gmail.com>
2024-07-21 11:25:20 -0500

    remove unused imports

--- feature/login/src/commonMain/kotlin/di/LoginDependencies.kt
+++ feature/login/src/commonMain/kotlin/di/LoginDependencies.kt
@@ -6,9 +6,7 @@ package org.pointyware.xyz.feature.login.di
 
 import org.koin.core.component.KoinComponent
 import org.koin.core.component.get
-import org.pointyware.xyz.feature.login.viewmodels.AccountCreationViewModel
 import org.pointyware.xyz.feature.login.viewmodels.AuthorizationViewModel
-import org.pointyware.xyz.feature.login.viewmodels.ProfileViewModel
 
 /**
  * Defines dependencies needed by all Login locations
