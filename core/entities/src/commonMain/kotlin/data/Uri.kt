1bbbc557cd7ee773705db6cb1f4f91a81265586a
Taush Sampley <taushsampley@gmail.com>
2024-07-22 10:00:32 -0500

    validate URI on creation
    * extract protocol and path

--- core/entities/src/commonMain/kotlin/data/Uri.kt
+++ core/entities/src/commonMain/kotlin/data/Uri.kt
@@ -10,8 +10,35 @@ package org.pointyware.xyz.core.entities.data
 class Uri(
     val value: String
 ) {
-
     init {
         require(value.isNotBlank()) { "Uri cannot be blank" }
+    }
+
+    sealed class Protocol(val value: String) {
+        data object File: Protocol("FILE")
+        data object Http: Protocol("HTTP")
+        data object Https: Protocol("HTTPS")
+        data object Ftp: Protocol("FTP")
+        class Other(value: String): Protocol(value)
+
+        companion object {
+            fun from(value: String): Protocol {
+                val normalized = value.uppercase()
+                listOf(Http, Https, Ftp).forEach {
+                    if (normalized == it.value) {
+                        return it
+                    }
+                }
+                return Other(normalized)
+            }
+        }
+    }
+    val protocol: Protocol = Protocol.from(value.substringBefore(protocolDelimiter))
+    val path: String = value.substringAfter(protocolDelimiter)
+
+    companion object {
+        const val protocolDelimiter = "://"
+
+        val nullUri = Uri("file:///dev/null")
     }
 }
