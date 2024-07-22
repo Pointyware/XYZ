c4173a9deba3446ccbee18fc7f70943a2ef90d7d
Taush Sampley <taushsampley@gmail.com>
2024-07-22 08:28:49 -0500

    implement profile creation view model

--- feature/login/src/commonMain/kotlin/viewmodels/ProfileCreationViewModel.kt
+++ feature/login/src/commonMain/kotlin/viewmodels/ProfileCreationViewModel.kt
@@ -7,53 +7,81 @@ package org.pointyware.xyz.feature.login.viewmodels
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
 import kotlinx.datetime.Instant
 import org.pointyware.xyz.core.entities.data.Uri
 import org.pointyware.xyz.core.entities.profile.Gender
+import org.pointyware.xyz.core.entities.profile.Name
 import org.pointyware.xyz.core.viewmodels.KoinViewModel
-import org.pointyware.xyz.core.viewmodels.ProfileUiState
 
 /**
  * Exposes general profile creation functionality.
  */
-interface ProfileCreationViewModel<out P:ProfileUiState> {
-    val state: StateFlow<P>
+interface ProfileCreationViewModel {
+    val state: StateFlow<ProfileCreationUiState>
     fun onProfileImageSelected(uri: Uri)
     fun onGivenNameChange(name: String)
     fun onMiddleNameChange(name: String)
     fun onFamilyNameChange(name: String)
     fun onBirthdateSelected(date: Instant?)
     fun onGenderSelected(gender: Gender)
-    fun onSubmit()
 }
 
-abstract class ProfileCreationViewModelImpl<P:ProfileUiState>(
-    initialState: P
-): KoinViewModel(), ProfileCreationViewModel<P> {
-    protected val mutableState = MutableStateFlow(initialState)
-    override val state: StateFlow<P> get() = mutableState.asStateFlow()
+abstract class ProfileCreationViewModelImpl(
+
+): KoinViewModel(), ProfileCreationViewModel {
+    protected val mutableState = MutableStateFlow(ProfileCreationUiState.empty)
+    override val state: StateFlow<ProfileCreationUiState> get() = mutableState.asStateFlow()
 
     override fun onProfileImageSelected(uri: Uri) {
-        TODO("Not yet implemented")
+        mutableState.update {
+            it.copy(image = uri)
+        }
     }
 
     override fun onGivenNameChange(name: String) {
-        TODO("Not yet implemented")
+        mutableState.update {
+            it.copy(fullName = it.fullName.copy(given = name))
+        }
     }
 
     override fun onMiddleNameChange(name: String) {
-        TODO("Not yet implemented")
+        mutableState.update {
+            it.copy(fullName = it.fullName.copy(middle = name))
+        }
     }
 
     override fun onFamilyNameChange(name: String) {
-        TODO("Not yet implemented")
+        mutableState.update {
+            it.copy(fullName = it.fullName.copy(family = name))
+        }
     }
 
     override fun onBirthdateSelected(date: Instant?) {
-        TODO("Not yet implemented")
+        mutableState.update {
+            it.copy(birthdate = date)
+        }
     }
 
     override fun onGenderSelected(gender: Gender) {
-        TODO("Not yet implemented")
+        mutableState.update {
+            it.copy(gender = gender)
+        }
+    }
+}
+
+data class ProfileCreationUiState(
+    val image: Uri,
+    val fullName: Name,
+    val birthdate: Instant?,
+    val gender: Gender,
+) {
+    companion object {
+        val empty = ProfileCreationUiState(
+            image = Uri(""),
+            fullName = Name("", "", ""),
+            birthdate = null,
+            gender = Gender.NotSpecified
+        )
     }
 }
