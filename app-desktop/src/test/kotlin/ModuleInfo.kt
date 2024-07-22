ceac0b9ad2d98550fba4a28f9ae4d2875c2cc45e
Taush Sampley <taushsampley@gmail.com>
2024-07-22 10:14:30 -0500

    add module info to test source set

---
+++ app-desktop/src/test/kotlin/ModuleInfo.kt
@@ -0,0 +1,7 @@
+/*
+ * Copyright (c) 2024 Pointyware. Use of this software is governed by the GPL-3.0 license.
+ */
+
+package org.pointyware.xyz.desktop
+
+const val MODULE_NAME = ":app-desktop/test"
