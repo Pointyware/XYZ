cc53e200f4387589c358192b50f37888f4720adc
Taush Sampley <taushsampley@gmail.com>
2024-07-22 13:19:21 -0500

    start domain boundary definitions

---
+++ core/entities/src/commonMain/kotlin/rides/README.md
@@ -0,0 +1,2 @@
+# Package org.pointyware.xyz.core.entities.rides
+This package contains entities relevant to rides, regardless of the user.
---
+++ core/entities/src/commonMain/kotlin/rides/RideRequest.kt
@@ -0,0 +1,11 @@
+/*
+ * Copyright (c) 2024 Pointyware. Use of this software is governed by the GPL-3.0 license.
+ */
+
+package org.pointyware.xyz.core.entities.rides
+
+/**
+ * A request for a ride made by a rider.
+ */
+interface RideRequest {
+}
