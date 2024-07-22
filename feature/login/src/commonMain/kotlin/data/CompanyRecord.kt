ce281263923c1a17d44a36888e700ae94f7382ff
Taush Sampley <taushsampley@gmail.com>
2024-07-22 13:51:39 -0500

    Add CompanyRecord to separate from entity

---
+++ feature/login/src/commonMain/kotlin/data/CompanyRecord.kt
@@ -0,0 +1,20 @@
+/*
+ * Copyright (c) 2024 Pointyware. Use of this software is governed by the GPL-3.0 license.
+ */
+
+package org.pointyware.xyz.feature.login.data
+
+import org.pointyware.xyz.core.entities.data.Uri
+import org.pointyware.xyz.core.entities.profile.PhoneNumber
+
+/**
+ * Separate from [org.pointyware.xyz.core.entities.business.Company] entity to allow representation without [org.pointyware.xyz.core.entities.profile.DriverProfile] details.
+ */
+data class CompanyRecord(
+    val name: String,
+    val tagline: String,
+    val bio: String,
+    val banner: Uri,
+    val logo: Uri,
+    val phone: PhoneNumber,
+)
