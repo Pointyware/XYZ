3c907831e62373521a6d45f78b2719eb1649009e
Taush Sampley <taushsampley@gmail.com>
2024-07-22 20:24:35 -0500

    Fix package

--- core/entities/src/commonMain/kotlin/ride/EndReason.kt
+++ core/entities/src/commonMain/kotlin/ride/EndReason.kt
@@ -2,7 +2,8 @@
  * Copyright (c) 2024 Pointyware. Use of this software is governed by the GPL-3.0 license.
  */
 
-package ride
+package org.pointyware.xyz.core.entities.ride
+
 enum class EndReason {
     Completed,
     RiderCanceled,
