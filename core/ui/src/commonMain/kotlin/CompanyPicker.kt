86b1351e30621c8227cafadf6a62b76d95911d6c
Taush Sampley <taushsampley@gmail.com>
2024-07-23 09:31:55 -0500

    Implement company picker

--- core/ui/src/commonMain/kotlin/CompanyPicker.kt
+++ core/ui/src/commonMain/kotlin/CompanyPicker.kt
@@ -4,20 +4,81 @@
 
 package org.pointyware.xyz.core.ui
 
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import org.pointyware.xyz.core.entities.Uuid
 import org.pointyware.xyz.core.viewmodels.drive.CompanyProfileUiState
+import org.pointyware.xyz.core.viewmodels.drive.CompanySelectionUiState
 
 /**
  * A search bar that allows the user to search for a company that shows up in a list or register a new company.
  */
 @Composable
 fun CompanyPicker(
-    value: CompanyProfileUiState,
+    state: CompanySelectionUiState,
     modifier: Modifier = Modifier,
-    onSelectCompany: (Uuid) -> Unit
+    onSearchChange: (String)->Unit,
+    onSelectCompany: (Uuid)->Unit,
+    onCreateCompany: ()->Unit
 ) {
-    TODO("Not yet Implemented" +
-            "")
+    var isMenuOpen by remember { mutableStateOf(false) }
+    Box(
+        modifier = modifier.clickable { isMenuOpen = true }
+    ) {
+        Row {
+            var query by remember { mutableStateOf("") }
+            TextField(
+                value = query,
+                label = { Text(text = "Company") },
+                onValueChange = { query = it },
+                keyboardActions = KeyboardActions(
+                    onDone = {
+                        onSearchChange(query)
+                        isMenuOpen = true
+                    }
+                )
+            )
+
+            IconButton(
+                onClick = {
+                    onSearchChange(query)
+                    isMenuOpen = true
+                }
+            ) {
+                Text(text = "Search")
+            }
+            IconButton(
+                onClick = onCreateCompany
+            ) {
+                Text(text = "New")
+            }
+        }
+
+        DropdownMenu(
+            expanded = isMenuOpen,
+            onDismissRequest = { isMenuOpen = false },
+        ) {
+            state.suggestions.forEach {
+                Text(
+                    text = it.name,
+                    modifier = Modifier.clickable {
+                        isMenuOpen = false
+                        onSelectCompany(it.id)
+                    }
+                )
+            }
+        }
+    }
 }
