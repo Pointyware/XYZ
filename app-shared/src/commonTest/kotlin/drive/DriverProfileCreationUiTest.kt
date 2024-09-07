ec09c4765dd01d77c47928cf5debfc5c50b0ac62
Taush Sampley <taushsampley@gmail.com>
2024-09-06 22:09:25 -0500

    Break up comment block

--- app-shared/src/commonTest/kotlin/drive/DriverProfileCreationUiTest.kt
+++ app-shared/src/commonTest/kotlin/drive/DriverProfileCreationUiTest.kt
@@ -6,8 +6,9 @@ package org.pointyware.xyz.app.drive
 
 import androidx.compose.ui.test.ExperimentalTestApi
 import androidx.compose.ui.test.runComposeUiTest
-import org.koin.core.KoinApplication
+import org.koin.core.context.stopKoin
 import org.koin.dsl.koinApplication
+import org.koin.mp.KoinPlatform.getKoin
 import org.pointyware.xyz.core.navigation.StackNavigationController
 import org.pointyware.xyz.core.ui.design.XyzTheme
 import org.pointyware.xyz.feature.login.DriverProfileCreationScreen
@@ -55,60 +56,82 @@ class DriverProfileCreationUiTest {
         Given:
         - User is logged out
         - Account does not exist for <email>
+         */
 
+        /*
         When:
         - User Opens App
         Then:
         - User is presented with Login Screen
+         */
 
+        /*
         When:
         - User Selects Create Account
         Then:
         - Login Screen shows Create Account Form
+         */
 
+        /*
         When:
         - User Enters <email>, <password>, <confirm password>
         Then:
         - Create Account Button is enabled
+         */
 
+        /*
         When:
         - User Selects Create Account
         Then:
         - Account is created for <email>
         - User is presented with Profile Creation Screen
         - Profile Creation Screen shows "Rider" Tab selected
+         */
 
+        /*
         When:
         - User Enters <given name>, <middle name>, <family name>
         Then:
         - Create Profile button is disabled
+         */
 
+        /*
         When:
         - User Enters <birthdate>
         Then:
         - Create Profile button is disabled
+         */
 
+        /*
         When:
         - User Selects <gender>
         Then:
         - Create Profile button is enabled
+         */
 
+        /*
         When:
         - User Taps "Driver" Tab
         Then:
         - Driver Profile Form is displayed
         - Company Picker shows "Independent" selected
+         */
 
+        /*
         When:
         - User Taps "Add Accommodation"
         Then:
         - Accommodation Picker is displayed
+         */
 
+        /*
         When:
         - User Selects <accommodation>
         Then:
         - Accommodation is added to Profile
+         */
 
+        /*
         When:
         - User Taps "Create Profile"
         Then:
