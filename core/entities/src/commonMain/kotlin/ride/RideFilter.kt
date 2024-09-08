35c490c84d8890ecd60c65b29df8d38b23e5226c
Taush Sampley <taushsampley@gmail.com>
2024-09-08 16:03:11 -0500

    Add `accepts(Ride)` function in RideFilter

--- core/entities/src/commonMain/kotlin/ride/RideFilter.kt
+++ core/entities/src/commonMain/kotlin/ride/RideFilter.kt
@@ -12,9 +12,43 @@ import org.pointyware.xyz.core.entities.geo.Length
  * Defines a filter for rides a driver will accept.
  */
 interface RideFilter {
-    val serviceArea: Area
-    val timeRange: ClosedRange<Instant>
-    val distanceRange: ClosedRange<Length>
-    val priceRange: ClosedRange<Currency>
-    val rateRange: ClosedRange<Currency>
+    fun accepts(candidate: Ride): Boolean
+
+    val serviceArea: Area?
+    val timeRange: ClosedRange<Instant>?
+    val distanceRange: ClosedRange<Length>?
+    val priceRange: ClosedRange<Currency>?
+    val rateRange: ClosedRange<Currency>?
+}
+
+private data class SimpleRideFilter(
+    override val serviceArea: Area? = null,
+    override val timeRange: ClosedRange<Instant>? = null,
+    override val distanceRange: ClosedRange<Length>? = null,
+    override val priceRange: ClosedRange<Currency>? = null,
+    override val rateRange: ClosedRange<Currency>? = null,
+): RideFilter {
+    override fun accepts(candidate: Ride): Boolean {
+//        if (priceRange != null && !priceRange.contains(candidate.)) {
+//            return false
+//        } TODO: add bid price to ride request
+//        if (rateRange != null && !rateRange.contains(candidate.rate)) {
+//            return false
+//        } TODO: add bid rate to ride request
+        val startCoordinates = candidate.plannedRoute?.start?.coordinates
+        if (serviceArea != null && startCoordinates != null && !serviceArea.contains(startCoordinates)) {
+            return false
+        }
+        val routeDistance = candidate.plannedRoute?.distance
+        if (distanceRange != null && routeDistance != null && !distanceRange.contains(routeDistance)) {
+            return false
+        }
+        val pickupTime = when (val status = candidate.status) { is Ride.Status.Scheduled -> status.timeToStart
+            else -> null
+        }
+        if (timeRange != null && pickupTime != null && !timeRange.contains(pickupTime)) {
+            return false
+        }
+        return true
+    }
 }
