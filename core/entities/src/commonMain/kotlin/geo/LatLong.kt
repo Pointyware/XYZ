2cbf9ef6eaac0950acc7e716f946c9ffdbc90d49
Taush Sampley <taushsampley@gmail.com>
2024-09-08 15:50:38 -0500

    Add distance calculation for lat long

--- core/entities/src/commonMain/kotlin/geo/LatLong.kt
+++ core/entities/src/commonMain/kotlin/geo/LatLong.kt
@@ -4,10 +4,39 @@
 
 package org.pointyware.xyz.core.entities.geo
 
+import kotlin.math.PI
+import kotlin.math.asin
+import kotlin.math.cos
+import kotlin.math.sin
+
+
+const val EARTH_RADIUS = 6371.0
+
 /**
  * A simple data class to represent a geographic location in latitude and longitude.
  */
 data class LatLong(
     val latitude: Double,
     val longitude: Double
-)
+) {
+    fun Double.toRadians(): Double {
+        return this * PI / 180
+    }
+
+    /**
+     * Calculate the distance between this location and another in kilometers assuming a
+     * spherical Earth of radius [EARTH_RADIUS] km.
+     */
+    fun distanceTo(coordinates: LatLong): Double {
+        val lat1 = latitude.toRadians()
+        val lon1 = longitude.toRadians()
+        val lat2 = coordinates.latitude.toRadians()
+        val lon2 = coordinates.longitude.toRadians()
+
+        val lonDif = lon2 - lon1
+
+        val centralAngle = asin(sin(lat1)*sin(lat2) + cos(lat1)*cos(lat2)*cos(lonDif))
+
+        return EARTH_RADIUS * centralAngle
+    }
+}
