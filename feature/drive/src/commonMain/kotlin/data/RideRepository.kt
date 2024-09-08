2adda48b8e9426f3c76aae24596ad923b17f8caa
Taush Sampley <taushsampley@gmail.com>
2024-09-08 14:31:31 -0500

    Create TestRideRepository
    * use naive levenshtein distance to sort returned locations

--- feature/drive/src/commonMain/kotlin/data/RideRepository.kt
+++ feature/drive/src/commonMain/kotlin/data/RideRepository.kt
@@ -5,6 +5,7 @@
 package org.pointyware.xyz.drive.data
 
 import kotlinx.coroutines.flow.Flow
+import org.pointyware.xyz.core.entities.ride.Location
 import org.pointyware.xyz.core.entities.ride.Ride
 import org.pointyware.xyz.core.entities.ride.RideFilter
 import org.pointyware.xyz.drive.local.RideCache
@@ -53,5 +54,66 @@ class RideRepositoryImpl(
 
     override suspend fun watchRides(filter: RideFilter): Result<Flow<Ride>> {
         return rideService.createRideFilter(filter)
+    }
+}
+
+class TestRideRepository(
+    val destinations: MutableSet<Location> = mutableSetOf(),
+): RideRepository {
+
+    private val maximumLevenshteinDistance = 20
+
+    /**
+     * Calculates the Levenshtein distance between this string and another.
+     * @return the number of insertions, deletions, and substitutions needed to transform this string into the other.
+     */
+    private fun String.levenshtein(other: String): Int {
+        val s = this
+        val t = other
+        val d = Array(s.length + 1) { IntArray(t.length + 1) }
+        for (i in 0..s.length) {
+            d[i][0] = i
+        }
+        for (j in 0..t.length) {
+            d[0][j] = j
+        }
+        for (i in 1..s.length) {
+            for (j in 1..t.length) {
+                val cost = if (s[i - 1] == t[j - 1]) 0 else 1
+                d[i][j] = minOf(
+                    d[i - 1][j] + 1,
+                    d[i][j - 1] + 1,
+                    d[i - 1][j - 1] + cost
+                )
+            }
+        }
+        return d[s.length][t.length]
+    }
+
+    override suspend fun searchDestinations(query: String): Result<RideSearchResult> {
+        val candidates = destinations.flatMap {
+            val score = it.name.levenshtein(query)
+            if (score < maximumLevenshteinDistance) {
+                listOf(it to score)
+            } else {
+                emptyList()
+            }
+        }.sortedBy { it.second }.map { it.first }
+
+        return Result.success(RideSearchResult(candidates))
+    }
+
+
+
+    override suspend fun postRide(ride: Ride): Result<Ride> {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun cancelRide(ride: Ride): Result<Ride> {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun watchRides(filter: RideFilter): Result<Flow<Ride>> {
+        TODO("Not yet implemented")
     }
 }
