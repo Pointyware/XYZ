7233637304d75fda3f453f2de58cf1c4ffe9ee57
Taush Sampley <taushsampley@gmail.com>
2024-09-08 20:38:03 -0500

    Add RideUiState doc and expand states

--- feature/ride/src/commonMain/kotlin/viewmodels/RideUiState.kt
+++ feature/ride/src/commonMain/kotlin/viewmodels/RideUiState.kt
@@ -6,15 +6,51 @@ package org.pointyware.xyz.feature.ride.viewmodels
 
 /**
  *
+ * - Idle: The rider is not currently requesting a ride.
+ * - Search: The rider is searching for a destination.
+ * - Confirm: The rider is confirming the ride details.
+ * - Posted: The rider has posted the ride and is waiting for a driver.
+ * - Waiting: The request has been accepted and the rider is waiting for the driver to arrive.
+ * - Riding: The rider is in the car and the ride is in progress.
  */
 sealed interface RideUiState {
 
+    /**
+     * The rider is not currently requesting a ride.
+     */
     data object Idle: RideUiState
 
+    /**
+     *
+     */
     data class Search(
         val query: String = "",
         val suggestions: List<String>
     ): RideUiState
 
+    data class Confirm(
+        val destination: String,
+        val price: Double
+    ): RideUiState
 
+    data class Posted(
+        val destination: String,
+        val price: Double
+    ): RideUiState
+
+    data class Waiting(
+        /**
+         * The driver's name. TODO: replace with BriefProfile?
+         */
+        val driver: String,
+        /**
+         * Driver's estimated time of arrival in minutes.
+         */
+        val eta: Int
+    ): RideUiState
+
+    data class Riding(
+        val driver: String,
+        val eta: Int
+    ): RideUiState
 }
