d8ca720c2c25e4d604e6530e089a4717c43b3777
Taush Sampley <taushsampley@gmail.com>
2024-09-09 14:14:37 -0500

    replace explicit koin startup with utility function

--- app-android/src/main/java/org/pointyware/xyz/android/XyzApplication.kt
+++ app-android/src/main/java/org/pointyware/xyz/android/XyzApplication.kt
@@ -9,12 +9,11 @@ import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.SupervisorJob
 import kotlinx.coroutines.launch
-import org.koin.core.context.startKoin
 import org.koin.core.parameter.parametersOf
 import org.koin.java.KoinJavaComponent.getKoin
 import org.pointyware.xyz.android.di.androidModule
 import org.pointyware.xyz.core.ui.ads.AdsController
-import org.pointyware.xyz.shared.di.appModule
+import org.pointyware.xyz.shared.di.setupKoin
 
 /**
  * This is the production XYZ application; it performs production environment setup.
@@ -23,12 +22,7 @@ class XyzApplication: Application() {
     override fun onCreate() {
         super.onCreate()
 
-        startKoin {
-            modules(
-                androidModule(),
-                appModule()
-            )
-        }
+        setupKoin(platformModule = androidModule())
 
         val koin = getKoin()
         val adsController = koin.get<AdsController> { parametersOf(this) }
