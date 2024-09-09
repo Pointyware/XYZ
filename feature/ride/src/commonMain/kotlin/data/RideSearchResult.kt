09de0ddcfe3d696422be9213d8d85a1f04e995ad
Taush Sampley <taushsampley@gmail.com>
2024-09-09 09:43:14 -0500

    Move RideSearchResult to :ride module

--- feature/ride/src/commonMain/kotlin/data/RideSearchResult.kt
+++ feature/ride/src/commonMain/kotlin/data/RideSearchResult.kt
@@ -2,7 +2,7 @@
  * Copyright (c) 2024 Pointyware. Use of this software is governed by the GPL-3.0 license.
  */
 
-package org.pointyware.xyz.drive.data
+package org.pointyware.xyz.feature.ride.data
 
 import org.pointyware.xyz.core.entities.ride.Location
 
