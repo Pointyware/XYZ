744c6a7c015b7aebb444bc8754522cbd044a08f4
Taush Sampley <taushsampley@gmail.com>
2024-09-09 15:16:22 -0500

    Refine RideUiState cases
    * Make driver BriefProfileUiState for details
    * add Route to Confirm, Posted, Waiting, and Riding classes
    * Add origin to Confirm class
    * Make price parameters type Currency

--- feature/ride/src/commonMain/kotlin/viewmodels/RideUiState.kt
+++ feature/ride/src/commonMain/kotlin/viewmodels/RideUiState.kt
@@ -4,7 +4,10 @@
 
 package org.pointyware.xyz.feature.ride.viewmodels
 
+import org.pointyware.xyz.core.entities.business.Currency
 import org.pointyware.xyz.core.entities.ride.Location
+import org.pointyware.xyz.core.entities.ride.Route
+import org.pointyware.xyz.core.viewmodels.BriefProfileUiState
 
 /**
  * Represents the state of a rider's UI.
@@ -28,16 +31,18 @@ sealed interface RideUiState {
      * The rider is confirming the ride details.
      */
     data class Confirm(
-        val destination: String,
-        val price: Double
+        val origin: Location,
+        val destination: Location,
+        val route: Route?,
+        val price: Currency?
     ): RideUiState
 
     /**
      * The rider has posted the ride and is waiting for a driver.
      */
     data class Posted(
-        val destination: String,
-        val price: Double
+        val route: Route,
+        val price: Currency
     ): RideUiState
 
     /**
@@ -45,20 +50,22 @@ sealed interface RideUiState {
      */
     data class Waiting(
         /**
-         * The driver's name. TODO: replace with BriefProfile?
+         * The driver's name.
          */
-        val driver: String,
+        val driver: BriefProfileUiState,
         /**
          * Driver's estimated time of arrival in minutes.
          */
-        val eta: Int
+        val eta: Int,
+        val route: Route,
     ): RideUiState
 
     /**
      * The rider is in the car and the ride is in progress.
      */
     data class Riding(
-        val driver: String,
+        val driver: BriefProfileUiState,
+        val route: Route,
         val eta: Int
     ): RideUiState
 }
