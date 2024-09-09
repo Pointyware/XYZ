678d42f3bb4774658217c0556a96a842937744ed
Taush Sampley <taushsampley@gmail.com>
2024-09-09 14:02:19 -0500

    Create utility function to centralize testing setup across platforms

--- app-shared/src/commonMain/kotlin/di/AppModule.kt
+++ app-shared/src/commonMain/kotlin/di/AppModule.kt
@@ -4,9 +4,13 @@
 
 package org.pointyware.xyz.shared.di
 
+import org.koin.core.context.loadKoinModules
+import org.koin.core.context.startKoin
+import org.koin.core.context.unloadKoinModules
 import org.koin.core.module.Module
 import org.koin.dsl.module
 import org.pointyware.xyz.core.data.di.coreDataModule
+import org.pointyware.xyz.core.entities.Uuid
 import org.pointyware.xyz.core.entities.di.coreEntitiesModule
 import org.pointyware.xyz.core.interactors.di.coreInteractorsModule
 import org.pointyware.xyz.core.local.di.coreLocalModule
@@ -16,9 +20,29 @@ import org.pointyware.xyz.core.ui.di.coreUiModule
 import org.pointyware.xyz.core.viewmodels.di.coreViewModelsModule
 import org.pointyware.xyz.drive.di.featureDriveModule
 import org.pointyware.xyz.feature.login.di.featureLoginModule
+import org.pointyware.xyz.feature.login.di.featureLoginRemoteModule
 import org.pointyware.xyz.feature.login.di.featureProfileModule
+import org.pointyware.xyz.feature.login.remote.AuthService
+import org.pointyware.xyz.feature.login.remote.TestAuthService
 import org.pointyware.xyz.feature.ride.di.featureRideModule
 
+
+fun setupKoin(platformModules: List<Module>) {
+    startKoin {
+        modules(appModule())
+        modules(platformModules)
+    }
+
+    // Temporary Test Setup - remove for production
+    unloadKoinModules(listOf(
+        featureLoginRemoteModule()
+    ))
+    loadKoinModules(module {
+        single<AuthService> { TestAuthService(
+            users = mutableMapOf("foo@bar.com" to TestAuthService.UserEntry("password", Uuid.v4()))
+        ) }
+    })
+}
 
 fun appModule(
     coreModule: Module = coreModule(),
