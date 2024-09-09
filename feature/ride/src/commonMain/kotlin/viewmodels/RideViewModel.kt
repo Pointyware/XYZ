5e4dc73c656a43d1eca33f971e78217b8a6801e8
Taush Sampley <taushsampley@gmail.com>
2024-09-08 20:38:11 -0500

    Stub RideViewModel

--- feature/ride/src/commonMain/kotlin/viewmodels/RideViewModel.kt
+++ feature/ride/src/commonMain/kotlin/viewmodels/RideViewModel.kt
@@ -6,10 +6,13 @@ package org.pointyware.xyz.feature.ride.viewmodels
 
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
+import org.pointyware.xyz.core.entities.ride.Location
 import org.pointyware.xyz.core.viewmodels.MapViewModelImpl
 
 /**
+ * Maintains the state of a rider UI and provides actions to update it.
  *
+ * @see RideUiState
  */
 class RideViewModel(
 
@@ -18,5 +21,23 @@ class RideViewModel(
     private val mutableState = MutableStateFlow<RideUiState>(RideUiState.Idle)
     val state: StateFlow<RideUiState> get() = mutableState
 
+    fun startSearch() {
+        TODO("State must be Idle; move to empty search state")
+    }
 
+    fun updateSearch(query: String) {
+        TODO("State must be Search; update search query; after a delay, send query for suggestions")
+    }
+
+    fun selectLocation(location: Location) {
+        TODO("State must be Search; select a location from the search results")
+    }
+
+    fun confirmDetails() {
+        TODO("State must be Confirm; confirm the ride details and post the ride")
+    }
+
+    fun cancelRide() {
+        TODO("State must be Waiting or Riding; cancel the ride")
+    }
 }
