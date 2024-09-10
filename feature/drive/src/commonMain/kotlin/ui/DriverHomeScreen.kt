fd4fd1016252d8a4faee886b02044742c442b5a3
Taush Sampley <taushsampley@gmail.com>
2024-09-09 22:46:04 -0500

    Create basic driver home screen and test

---
+++ feature/drive/src/commonMain/kotlin/ui/DriverHomeScreen.kt
@@ -0,0 +1,33 @@
+/*
+ * Copyright (c) 2024 Pointyware. Use of this software is governed by the GPL-3.0 license.
+ */
+
+package org.pointyware.xyz.drive.ui
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import org.pointyware.xyz.core.navigation.XyzNavController
+import org.pointyware.xyz.drive.navigation.driverActiveRoute
+
+/**
+ */
+@Composable
+fun DriverHomeScreen(
+    navController: XyzNavController,
+) {
+    Box(
+        modifier = Modifier.fillMaxSize()
+    ) {
+        Button(
+            onClick = {
+                navController.navigateTo(driverActiveRoute)
+            }
+        ) {
+            Text("Start Taking Requests")
+        }
+    }
+}
