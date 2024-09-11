f87dbdd06cd63c5d2bfd5d8bac773678e4e3f17b
Taush Sampley <taushsampley@gmail.com>
2024-09-10 19:58:44 -0500

    Map repository state to view model state

--- feature/drive/src/commonMain/kotlin/viewmodels/DriveViewModel.kt
+++ feature/drive/src/commonMain/kotlin/viewmodels/DriveViewModel.kt
@@ -4,28 +4,82 @@
 
 package org.pointyware.xyz.drive.viewmodels
 
+import kotlinx.coroutines.Job
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
 import org.pointyware.xyz.core.entities.Uuid
 import org.pointyware.xyz.core.viewmodels.MapViewModelImpl
+import org.pointyware.xyz.drive.SimpleRideFilter
+import org.pointyware.xyz.drive.data.RideRepository
 import org.pointyware.xyz.drive.ui.DriveScreenState
 
 /**
  *
  */
 class DriveViewModel(
-
+    private val repository: RideRepository
 ): MapViewModelImpl() {
 
-    private val mutableState = MutableStateFlow<DriveScreenState>(DriveScreenState.Idle)
+    private val mutableState = MutableStateFlow<DriveScreenState>(DriveScreenState.AvailableRequests(emptyList()))
     val state: StateFlow<DriveScreenState> get() = mutableState
 
+    init {
+        watchRequests()
+    }
+
+    private var requestsJob: Job? = null
+    private fun watchRequests() {
+        requestsJob?.cancel()
+        requestsJob = viewModelScope.launch {
+            repository.watchRequests(SimpleRideFilter.Permissive)
+                .onSuccess { flow ->
+                    flow.collect { requestList ->
+                        mutableState.value = DriveScreenState.AvailableRequests(
+                            requests = requestList.map {
+                                RideRequestUiStateImpl(
+                                    requestId = it.rideId,
+                                    destination = it.route.end,
+                                    distanceFromDriver = it.route.distance,
+                                    distanceOfRoute = it.route.distance,
+                                    riderServiceRate = it.rate
+                                )
+                            }
+                        )
+                    }
+                }
+                .onFailure {
+                    TODO("Handle failure")
+                }
+        }
+    }
+
+    private fun stopWatchingRequests() {
+        requestsJob?.cancel()
+    }
+
     fun onAccept(request: Uuid) {
-        TODO("Send Acceptance; Update State on Success")
+        viewModelScope.launch {
+            repository.acceptRequest(request)
+                .onSuccess {
+                    stopWatchingRequests()
+                    mutableState.value = DriveScreenState.Accepted(it)
+                }
+                .onFailure {
+                    TODO("Handle failure")
+                }
+        }
     }
 
     fun onReject(request: Uuid) {
-        TODO("Send Rejection; Update State on Success")
-    }
+        viewModelScope.launch {
+            repository.rejectRequest(request)
+                .onSuccess {
 
+                }
+                .onFailure {
+                    TODO("Handle failure")
+                }
+        }
+    }
 }
