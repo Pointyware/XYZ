f81a9bb2f588357ec5adb24bd2d1f94efff8f6d3
Taush Sampley <taushsampley@gmail.com>
2024-09-10 19:08:29 -0500

    Add id and rate to ride request ui state

--- feature/drive/src/commonMain/kotlin/viewmodels/RideRequestUiState.kt
+++ feature/drive/src/commonMain/kotlin/viewmodels/RideRequestUiState.kt
@@ -4,7 +4,9 @@
 
 package org.pointyware.xyz.drive.viewmodels
 
+import org.pointyware.xyz.core.entities.Uuid
 import org.pointyware.xyz.core.entities.business.Currency
+import org.pointyware.xyz.core.entities.business.Rate
 import org.pointyware.xyz.core.entities.geo.Length
 import org.pointyware.xyz.core.entities.geo.Location
 
@@ -12,9 +14,21 @@ import org.pointyware.xyz.core.entities.geo.Location
  * Displays information about a new ride request: distance from driver, distance of route, and rider service rate.
  */
 interface RideRequestUiState {
+    val requestId: Uuid
     val destination: Location
     val distanceFromDriver: Length
     val distanceOfRoute: Length
-//    val riderServiceRate: Currency // TODO: create rate entity
+    val riderServiceRate: Rate
     val totalFair: Currency
+}
+
+data class RideRequestUiStateImpl(
+    override val requestId: Uuid,
+    override val destination: Location,
+    override val distanceFromDriver: Length,
+    override val distanceOfRoute: Length,
+    override val riderServiceRate: Rate,
+): RideRequestUiState {
+    override val totalFair: Currency
+        get() = riderServiceRate * distanceOfRoute
 }
