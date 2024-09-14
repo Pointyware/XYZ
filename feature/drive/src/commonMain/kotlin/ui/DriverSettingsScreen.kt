8a4f2aca0d00532137663ba292f7094e4b4c42c4
Taush Sampley <taushsampley@gmail.com>
2024-09-13 19:07:17 -0500

    Create DriverSettingsScreen

---
+++ feature/drive/src/commonMain/kotlin/ui/DriverSettingsScreen.kt
@@ -0,0 +1,55 @@
+/*
+ * Copyright (c) 2024 Pointyware. Use of this software is governed by the GPL-3.0 license.
+ */
+
+package org.pointyware.xyz.drive.ui
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import org.pointyware.xyz.drive.viewmodels.DriverSettingsViewModel
+
+/**
+ *
+ */
+@Composable
+fun DriverSettingsScreen(
+    viewModel: DriverSettingsViewModel
+) {
+    val state by viewModel.state.collectAsState()
+
+    Column {
+        TextField(
+            label = { Text("Cost of Maintenance") },
+            value = state.maintenanceCost,
+            onValueChange = viewModel::onUpdateCostOfMaintenance,
+        )
+        TextField(
+            label = { Text("Price per Mile (Pickup)") },
+            value = state.pickupCost,
+            onValueChange = viewModel::onUpdatePickupPricePerMile,
+        )
+        TextField(
+            label = { Text("Price per Mile (Dropoff)") },
+            value = state.dropoffCost,
+            onValueChange = viewModel::onUpdateDropoffPricePerMile,
+        )
+        Row {
+            Button(
+                onClick = viewModel::onRevertChanges
+            ) {
+                Text("Revert")
+            }
+            Button(
+                onClick = viewModel::onSubmitChanges
+            ) {
+                Text("Save")
+            }
+        }
+    }
+}
