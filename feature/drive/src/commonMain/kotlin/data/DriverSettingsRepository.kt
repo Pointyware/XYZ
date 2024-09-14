feabb820c878a0ae30629db961c1f08007c90bae
Taush Sampley <taushsampley@gmail.com>
2024-09-13 19:03:41 -0500

    Stub driver settings impl and implement naive test repo

--- feature/drive/src/commonMain/kotlin/data/DriverSettingsRepository.kt
+++ feature/drive/src/commonMain/kotlin/data/DriverSettingsRepository.kt
@@ -4,8 +4,12 @@
 
 package org.pointyware.xyz.drive.data
 
+import org.pointyware.xyz.core.entities.business.Rate.Companion.div
+import org.pointyware.xyz.core.entities.business.dollarCents
 import org.pointyware.xyz.core.entities.geo.LatLong
+import org.pointyware.xyz.core.entities.geo.LengthUnit
 import org.pointyware.xyz.drive.RideFilter
+import org.pointyware.xyz.drive.SimpleRideFilter
 import org.pointyware.xyz.drive.entities.DriverRates
 
 /**
@@ -31,4 +35,68 @@ interface DriverSettingsRepository {
 
     fun getDriverLocation(): LatLong
     fun setDriverLocation(location: LatLong)
+}
+
+class DriverSettingsRepositoryImpl(
+
+): DriverSettingsRepository {
+    override fun getFilter(): RideFilter {
+        TODO("Not yet implemented")
+    }
+
+    override fun setFilter(filter: RideFilter) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getDriverRates(): DriverRates {
+        TODO("Not yet implemented")
+    }
+
+    override fun setDriverRates(rates: DriverRates) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getDriverLocation(): LatLong {
+        TODO("Not yet implemented")
+    }
+
+    override fun setDriverLocation(location: LatLong) {
+        TODO("Not yet implemented")
+    }
+}
+
+class TestDriverSettingsRepository(
+
+): DriverSettingsRepository {
+
+    private var filter: RideFilter = SimpleRideFilter.Permissive
+    override fun getFilter(): RideFilter {
+        return filter
+    }
+
+    override fun setFilter(filter: RideFilter) {
+        this.filter = filter
+    }
+
+    private var rates: DriverRates = DriverRates(
+        maintenanceCost = 0L.dollarCents() / LengthUnit.MILES,
+        pickupCost = 0L.dollarCents() / LengthUnit.MILES,
+        dropoffCost = 0L.dollarCents() / LengthUnit.MILES
+    )
+    override fun getDriverRates(): DriverRates {
+        return rates
+    }
+
+    override fun setDriverRates(rates: DriverRates) {
+        this.rates = rates
+    }
+
+    private var location: LatLong = LatLong(0.0, 0.0)
+    override fun getDriverLocation(): LatLong {
+        return location
+    }
+
+    override fun setDriverLocation(location: LatLong) {
+        this.location = location
+    }
 }
