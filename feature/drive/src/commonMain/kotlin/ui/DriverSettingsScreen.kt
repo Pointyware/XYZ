b162d695a47e7a45388a4d9234f9b69c1135a626
Taush Sampley <taushsampley@gmail.com>
2024-09-13 19:48:09 -0500

    Add semantics labels

--- feature/drive/src/commonMain/kotlin/ui/DriverSettingsScreen.kt
+++ feature/drive/src/commonMain/kotlin/ui/DriverSettingsScreen.kt
@@ -12,6 +12,9 @@ import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.semantics.contentDescription
+import androidx.compose.ui.semantics.semantics
 import org.pointyware.xyz.drive.viewmodels.DriverSettingsViewModel
 
 /**
@@ -25,16 +28,19 @@ fun DriverSettingsScreen(
 
     Column {
         TextField(
+            modifier = Modifier.semantics { contentDescription = "Cost of Maintenance" },
             label = { Text("Cost of Maintenance") },
             value = state.maintenanceCost,
             onValueChange = viewModel::onUpdateCostOfMaintenance,
         )
         TextField(
+            modifier = Modifier.semantics { contentDescription = "Pickup Rate" },
             label = { Text("Price per Mile (Pickup)") },
             value = state.pickupCost,
             onValueChange = viewModel::onUpdatePickupPricePerMile,
         )
         TextField(
+            modifier = Modifier.semantics { contentDescription = "Dropoff Rate" },
             label = { Text("Price per Mile (Dropoff)") },
             value = state.dropoffCost,
             onValueChange = viewModel::onUpdateDropoffPricePerMile,
