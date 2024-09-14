92811db386889a33e31dd2014ca1fd50d78b687a
Taush Sampley <taushsampley@gmail.com>
2024-09-13 19:50:22 -0500

    Set input keyboard options to decimal

--- feature/drive/src/commonMain/kotlin/ui/DriverSettingsScreen.kt
+++ feature/drive/src/commonMain/kotlin/ui/DriverSettingsScreen.kt
@@ -6,6 +6,7 @@ package org.pointyware.xyz.drive.ui
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
@@ -15,6 +16,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.semantics.contentDescription
 import androidx.compose.ui.semantics.semantics
+import androidx.compose.ui.text.input.KeyboardType
 import org.pointyware.xyz.drive.viewmodels.DriverSettingsViewModel
 
 /**
@@ -31,18 +33,21 @@ fun DriverSettingsScreen(
             modifier = Modifier.semantics { contentDescription = "Cost of Maintenance" },
             label = { Text("Cost of Maintenance") },
             value = state.maintenanceCost,
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Decimal),
             onValueChange = viewModel::onUpdateCostOfMaintenance,
         )
         TextField(
             modifier = Modifier.semantics { contentDescription = "Pickup Rate" },
             label = { Text("Price per Mile (Pickup)") },
             value = state.pickupCost,
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Decimal),
             onValueChange = viewModel::onUpdatePickupPricePerMile,
         )
         TextField(
             modifier = Modifier.semantics { contentDescription = "Dropoff Rate" },
             label = { Text("Price per Mile (Dropoff)") },
             value = state.dropoffCost,
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Decimal),
             onValueChange = viewModel::onUpdateDropoffPricePerMile,
         )
         Row {
