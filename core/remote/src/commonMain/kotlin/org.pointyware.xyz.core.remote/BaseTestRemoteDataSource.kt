550b7e3b5803ad9ef636716d948dd6c49fae3c06
Taush Sampley <taushsampley@gmail.com>
2024-09-15 20:25:37 -0500

    fix duplicate package path

--- core/remote/src/commonMain/kotlin/org.pointyware.xyz.core.remote/BaseTestRemoteDataSource.kt
+++ core/remote/src/commonMain/kotlin/org.pointyware.xyz.core.remote/BaseTestRemoteDataSource.kt
@@ -2,7 +2,7 @@
  * Copyright (c) 2024 Pointyware. Use of this software is governed by the GPL-3.0 license.
  */
 
-package org.pointyware.xyz.core.remote.org.pointyware.xyz.core.remote
+package org.pointyware.xyz.core.remote
 
 /**
  * Simulates a remote data source, i.e. a network API.
