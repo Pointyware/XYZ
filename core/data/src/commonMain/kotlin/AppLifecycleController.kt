b2591f6c2cc7e86d3c7654ffc4c8b3ea0863d4db
Taush Sampley <taushsampley@gmail.com>
2024-09-16 20:10:31 -0500

    Add Create lifecycle event and create implement default

--- core/data/src/commonMain/kotlin/AppLifecycleController.kt
+++ core/data/src/commonMain/kotlin/AppLifecycleController.kt
@@ -4,16 +4,18 @@
 
 package org.pointyware.xyz.core.data
 
-import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharedFlow
+import kotlinx.coroutines.flow.asStateFlow
 
 /**
  * Allows for lifecycle events to be handled by the app.
  */
 interface AppLifecycleController {
     /**
-     * A state flow of lifecycle events.
+     * A flow of lifecycle events. Will replay the latest event on subscription.
      */
-    val lifecycleObservable: StateFlow<LifecycleEvent>
+    val lifecycleState: SharedFlow<LifecycleEvent>
 
     /**
      * Called when the app is started.
@@ -37,8 +39,32 @@ interface AppLifecycleController {
 }
 
 enum class LifecycleEvent {
+    Create,
     Start,
     Resume,
     Pause,
     Stop
+}
+
+class DefaultAppLifecycleController : AppLifecycleController {
+
+    private val mutableLifecycleState = MutableStateFlow(LifecycleEvent.Create)
+    override val lifecycleState: SharedFlow<LifecycleEvent>
+        get() = mutableLifecycleState.asStateFlow()
+
+    override fun onAppStart() {
+        mutableLifecycleState.value = LifecycleEvent.Start
+    }
+
+    override fun onAppResume() {
+        mutableLifecycleState.value = LifecycleEvent.Resume
+    }
+
+    override fun onAppPause() {
+        mutableLifecycleState.value = LifecycleEvent.Pause
+    }
+
+    override fun onAppStop() {
+        mutableLifecycleState.value = LifecycleEvent.Stop
+    }
 }
