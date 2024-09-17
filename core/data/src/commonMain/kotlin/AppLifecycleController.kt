63ba5e9dceeb0fa6cc6725ff172232a1e066d2fb
Taush Sampley <taushsampley@gmail.com>
2024-09-16 20:00:15 -0500

    Add trivial docs

--- core/data/src/commonMain/kotlin/AppLifecycleController.kt
+++ core/data/src/commonMain/kotlin/AppLifecycleController.kt
@@ -4,12 +4,41 @@
 
 package org.pointyware.xyz.core.data
 
+import kotlinx.coroutines.flow.StateFlow
+
 /**
  * Allows for lifecycle events to be handled by the app.
  */
 interface AppLifecycleController {
+    /**
+     * A state flow of lifecycle events.
+     */
+    val lifecycleObservable: StateFlow<LifecycleEvent>
+
+    /**
+     * Called when the app is started.
+     */
     fun onAppStart()
+
+    /**
+     * Called when the app is resumed.
+     */
     fun onAppResume()
+
+    /**
+     * Called when the app is paused.
+     */
     fun onAppPause()
+
+    /**
+     * Called when the app is stopped.
+     */
     fun onAppStop()
+}
+
+enum class LifecycleEvent {
+    Start,
+    Resume,
+    Pause,
+    Stop
 }
