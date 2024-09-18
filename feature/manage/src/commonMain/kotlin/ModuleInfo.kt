5f72b56180c8ba15a1e75eba4febe0e87fd4ecdf
Taush Sampley <taushsampley@gmail.com>
2024-09-18 00:19:24 -0500

    remove module info files

--- feature/manage/src/commonMain/kotlin/ModuleInfo.kt
+++
@@ -1,7 +0,0 @@
-/*
- * Copyright (c) 2024 Pointyware. Use of this software is governed by the GPL-3.0 license.
- */
-
-package org.pointyware.xyz.feature.manage
-
-const val MODULE_NAME = ":feature:manage/commonMain"
