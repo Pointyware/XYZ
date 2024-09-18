0ba7dc4bc19c6f28ef127e1462c944ed114e3bea
Taush Sampley <taushsampley@gmail.com>
2024-09-18 09:54:59 -0500

    Write KoinComponentsTest with expected behavior

--- core/data/src/commonTest/kotlin/org.pointyware.xyz.core.data/KoinComponentsTest.kt
+++ core/data/src/commonTest/kotlin/org.pointyware.xyz.core.data/KoinComponentsTest.kt
@@ -7,9 +7,13 @@ package org.pointyware.xyz.core.data
 import io.ktor.http.ContentType
 import org.koin.core.context.startKoin
 import org.koin.core.context.stopKoin
+import org.koin.core.module.dsl.scopedOf
 import org.koin.dsl.module
 import org.koin.mp.KoinPlatform.getKoin
 import org.pointyware.xyz.core.data.di.ApplicationComponent
+import org.pointyware.xyz.core.data.di.ViewComponent
+import org.pointyware.xyz.core.data.di.ViewModelComponent
+import org.pointyware.xyz.core.data.di.WindowComponent
 import kotlin.test.AfterTest
 import kotlin.test.BeforeTest
 import kotlin.test.Test
@@ -19,13 +23,27 @@ import kotlin.test.Test
  */
 class KoinComponentsTest {
 
+    class AppDep()
+    class WindowDep(val appDep: AppDep)
+    class ViewModelDep(val windowDep: WindowDep)
+    class ViewDep(val viewModelDep: ViewModelDep)
+
     @BeforeTest
     fun setUp() {
         startKoin {
             modules(
                 module {
                     scope<ApplicationComponent> {
-
+                        scopedOf(::AppDep)
+                    }
+                    scope<WindowComponent> {
+                        scopedOf(::WindowDep)
+                    }
+                    scope<ViewModelComponent> {
+                        scopedOf(::ViewModelDep)
+                    }
+                    scope<ViewComponent> {
+                        scopedOf(::ViewDep)
                     }
                 }
             )
@@ -42,7 +60,15 @@ class KoinComponentsTest {
         val koin = getKoin()
 
         val appComponent = ApplicationComponent()
+        appComponent.scope.get<AppDep>()
 
+        val windowComponent = WindowComponent(appComponent)
+        windowComponent.scope.get<WindowDep>()
 
+        val viewModelComponent = ViewModelComponent(windowComponent)
+        viewModelComponent.scope.get<ViewModelDep>()
+
+        val viewComponent = ViewComponent(viewModelComponent)
+        viewComponent.scope.get<ViewDep>()
     }
 }
