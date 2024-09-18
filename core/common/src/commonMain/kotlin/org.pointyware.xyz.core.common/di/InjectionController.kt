bd97a12203f70513ee8ba932ca2a525b1771c100
Taush Sampley <taushsampley@gmail.com>
2024-09-18 14:55:21 -0500

    Replace environment with BuildType

--- core/common/src/commonMain/kotlin/org.pointyware.xyz.core.common/di/InjectionController.kt
+++ core/common/src/commonMain/kotlin/org.pointyware.xyz.core.common/di/InjectionController.kt
@@ -4,31 +4,29 @@
 
 package org.pointyware.xyz.core.common.di
 
-enum class Environment {
-    DEV, STAGE, PROD
-}
+import org.pointyware.xyz.core.common.BuildType
 
 /**
  * Controls the injection of objects based on the current environment setting.
  */
 interface InjectionController {
 
-    var environment: Environment
+    var environment: BuildType
 
     /**
      * Returns an instance of the object based on the current environment setting. If a factory is not found for the current environment, the next environment in the list is used. In order: dev, stage, prod.
      * @throws IllegalStateException if no factory is found for the current environment or any of the subsequent environments.
      */
-    fun <T> inject(factoryMap: Map<Environment, ()->T>): T
+    fun <T> inject(factoryMap: Map<BuildType, ()->T>): T
 }
 
 internal class InjectionControllerImpl: InjectionController {
 
-    override var environment: Environment = Environment.DEV
+    override var environment: BuildType = BuildType.DEBUG
 
-    override fun <T> inject(factoryMap: Map<Environment, () -> T>): T {
-        for (index in environment.ordinal until Environment.entries.size) {
-            val environment = Environment.entries[index]
+    override fun <T> inject(factoryMap: Map<BuildType, () -> T>): T {
+        for (index in environment.ordinal until BuildType.entries.size) {
+            val environment = BuildType.entries[index]
             val factory = factoryMap[environment]
             if (factory != null) {
                 return factory.invoke()
@@ -40,4 +38,4 @@ internal class InjectionControllerImpl: InjectionController {
 
 private val singletonInjectionController: InjectionController = InjectionControllerImpl()
 
-fun <T> inject(factoryMap: Map<Environment, ()->T>): T = singletonInjectionController.inject(factoryMap)
+fun <T> inject(factoryMap: Map<BuildType, ()->T>): T = singletonInjectionController.inject(factoryMap)
