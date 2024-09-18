bf28b92fd85b0ddedfbb806ca03e5ee874ac5366
Taush Sampley <taushsampley@gmail.com>
2024-09-18 16:47:39 -0500

    Add koin component extension

---
+++ core/common/src/commonMain/kotlin/org.pointyware.xyz.core.common/di/KoinComponentExt.kt
@@ -0,0 +1,22 @@
+/*
+ * Copyright (c) 2024 Pointyware. Use of this software is governed by the GPL-3.0 license.
+ */
+
+package org.pointyware.xyz.core.common.di
+
+import org.koin.core.component.KoinScopeComponent
+import org.koin.core.component.createScope
+import org.koin.core.scope.Scope
+
+/**
+ * Create a nested scope for the component
+ */
+fun KoinScopeComponent.nestedScope(parent: KoinScopeComponent? = null): Lazy<Scope> {
+    return lazy {
+        createScope(this).also {
+            parent?.let { parent ->
+                it.linkTo(parent.scope)
+            }
+        }
+    }
+}
