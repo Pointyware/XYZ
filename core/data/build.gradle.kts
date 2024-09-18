3976a5418cc25317be9cb7530948e89587e8f963
Taush Sampley <taushsampley@gmail.com>
2024-09-18 09:35:33 -0500

    Add koin testing to :core:data/test sources

--- core/data/build.gradle.kts
+++ core/data/build.gradle.kts
@@ -52,6 +52,7 @@ kotlin {
         val commonTest by getting {
             dependencies {
                 implementation(libs.kotlin.test)
+                implementation(libs.koin.test)
             }
         }
 
