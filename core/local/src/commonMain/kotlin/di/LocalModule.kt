7f9df762ac4dc46eb1116293862076611a7eaa1e
Taush Sampley <taushsampley@gmail.com>
2024-09-17 20:44:31 -0500

    provide test directory if build is in debug mode

--- core/local/src/commonMain/kotlin/di/LocalModule.kt
+++ core/local/src/commonMain/kotlin/di/LocalModule.kt
@@ -4,10 +4,25 @@
 
 package org.pointyware.xyz.core.local.di
 
+import kotlinx.io.files.Path
+import kotlinx.io.files.SystemFileSystem
+import org.koin.core.qualifier.named
 import org.koin.dsl.module
+import org.pointyware.xyz.core.common.BuildInfo
+
+
+val testDirectory = named("test-directory")
 
 /**
  *
  */
 fun coreLocalModule() = module {
+
+    if (BuildInfo.isDebug) {
+        factory<Path>(qualifier = testDirectory) {
+            Path("testing").also {
+                SystemFileSystem.createDirectories(it)
+            }
+        }
+    }
 }
