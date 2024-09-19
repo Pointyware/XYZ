dc1b3755ab30fe48b37f48d1f14209570e1d9bbe
Taush Sampley <taushsampley@gmail.com>
2024-09-19 15:56:02 -0500

    Make business hierarchy serializable

--- core/entities/src/commonMain/kotlin/business/Company.kt
+++ core/entities/src/commonMain/kotlin/business/Company.kt
@@ -4,6 +4,7 @@
 
 package org.pointyware.xyz.core.entities.business
 
+import kotlinx.serialization.Serializable
 import org.pointyware.xyz.core.entities.profile.PhoneNumber
 import org.pointyware.xyz.core.entities.data.Uri
 
@@ -13,6 +14,7 @@ import org.pointyware.xyz.core.entities.data.Uri
  * Etymologically, refers to what you do with your time: "busy-ness", from Old English "bisig", meaning "anxiety" or "care".
  * - https://www.etymonline.com/word/business
  */
+@Serializable
 sealed interface Business {
 
 }
@@ -20,6 +22,7 @@ sealed interface Business {
 /**
  * A business owned and operated by a single individual. No special legal status.
  */
+@Serializable
 data object Individual : Business
 
 /**
@@ -33,11 +36,12 @@ data object Individual : Business
  * Etymologically, refers to a "bread fellow", one with whom you are intimate.
  * - https://www.etymonline.com/word/company
  */
-interface Company : Business {
-    val name: String
-    val tagline: String
-    val bio: String
-    val banner: Uri
-    val logo: Uri
-    val phone: PhoneNumber
-}
+@Serializable
+data class Company(
+    val name: String,
+    val tagline: String,
+    val bio: String,
+    val banner: Uri,
+    val logo: Uri,
+    val phone: PhoneNumber,
+): Business
