a5d01963ef00bb1bf608c79d2274c6af2b5c6596
Taush Sampley <taushsampley@gmail.com>
2024-09-19 14:24:40 -0500

    Write simple file writing test for FakeAuthService

--- feature/login/src/commonTest/kotlin/org/pointyware/xyz/feature/login/remote/fake/FakeAuthServiceTest.kt
+++ feature/login/src/commonTest/kotlin/org/pointyware/xyz/feature/login/remote/fake/FakeAuthServiceTest.kt
@@ -4,23 +4,52 @@
 
 package org.pointyware.xyz.feature.login.remote.fake
 
-import kotlinx.io.files.FileSystem
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.IO
+import kotlinx.coroutines.test.runTest
+import kotlinx.io.Buffer
+import kotlinx.io.files.Path
 import kotlinx.io.files.SystemFileSystem
 import kotlinx.io.files.SystemTemporaryDirectory
+import kotlinx.io.readByteArray
+import kotlinx.serialization.json.Json
+import org.pointyware.xyz.core.data.DefaultLifecycleController
+import org.pointyware.xyz.core.data.LifecycleController
+import org.pointyware.xyz.core.entities.Uuid
+import org.pointyware.xyz.core.entities.profile.Profile
 import kotlin.test.AfterTest
 import kotlin.test.BeforeTest
 import kotlin.test.Test
+import kotlin.test.assertEquals
+import kotlin.test.assertTrue
 
 /**
- * TODO: describe purpose/intent of FakeProfileServiceTest
+ *
  */
 class FakeAuthServiceTest {
 
-    lateinit var fakeProfileService: FakeProfileService
+    lateinit var accountsFile: Path
+    lateinit var lifecycleController: LifecycleController
+
+    lateinit var fakeAuthService: FakeAuthService
 
     @BeforeTest
     fun setUp() {
         val tempDirectory = SystemTemporaryDirectory
+        accountsFile = Path(tempDirectory, "accounts.json")
+        lifecycleController = DefaultLifecycleController()
+
+        fakeAuthService = FakeAuthService(
+            accountsFile = accountsFile,
+            users = mutableMapOf(),
+
+            json = Json { isLenient = true },
+
+            lifecycleController = lifecycleController,
+            dataContext = Dispatchers.IO,
+            dataScope = CoroutineScope(Dispatchers.IO)
+        )
     }
 
     @AfterTest
@@ -34,9 +63,27 @@ class FakeAuthServiceTest {
     }
 
     @Test
-    fun `fake should save users when lifecycle controller emits onStop`() {
+    fun `fake should save users when lifecycle controller emits onStop`()  = runTest {
         /*
-        Given
+        Given:
+        - some email and password
+        - a FakeAuthService
+        - a lifecycle controller
          */
+        val email = "driver1@some.com"
+        val password = "password1"
+        fakeAuthService.createUser(email, password)
+
+        /*
+        When:
+        - the lifecycle controller emits Stop
+         */
+        lifecycleController.onStop()
+
+        /*
+        Then:
+        - the FakeProfileService should save the users
+         */
+        assertTrue(SystemFileSystem.exists(accountsFile))
     }
 }
