caab4259c9cf632666a9e05df2969017bc2004a2
Taush Sampley <taushsampley@gmail.com>
2024-09-18 20:22:59 -0500

    add experimental component hierarchy

--- app-shared/src/commonMain/kotlin/di/AppModule.kt
+++ app-shared/src/commonMain/kotlin/di/AppModule.kt
@@ -8,7 +8,12 @@ import org.koin.core.context.startKoin
 import org.koin.core.module.Module
 import org.koin.core.module.dsl.bind
 import org.koin.core.module.dsl.factoryOf
+import org.koin.core.module.dsl.singleOf
 import org.koin.dsl.module
+import org.pointyware.xyz.core.common.di.ApplicationComponent
+import org.pointyware.xyz.core.common.di.ViewComponent
+import org.pointyware.xyz.core.common.di.ViewModelComponent
+import org.pointyware.xyz.core.common.di.WindowComponent
 import org.pointyware.xyz.core.data.di.coreDataModule
 import org.pointyware.xyz.core.entities.di.coreEntitiesModule
 import org.pointyware.xyz.core.interactors.di.coreInteractorsModule
@@ -36,6 +41,22 @@ fun appModule(
     coreModule: Module = coreModule(),
     featureModule: Module = featureModule(),
 ): Module = module {
+    singleOf(::ApplicationComponent) // only one application
+
+    scope<ApplicationComponent> {
+        factoryOf(::WindowComponent) // multiple windows but each refers to scope application
+    }
+    scope<WindowComponent> {
+        factoryOf(::ViewModelComponent) // multiple view models but each refers to scope window
+        factoryOf(::ViewComponent) // multiple views but each refers to scope window
+    }
+    scope<ViewModelComponent> {
+        // dependencies used only within view models
+    }
+    scope<ViewComponent> {
+        // dependencies used only within views
+    }
+
     factoryOf(::KoinAppDependencies) { bind<AppDependencies>() }
     includes(
         coreModule,
