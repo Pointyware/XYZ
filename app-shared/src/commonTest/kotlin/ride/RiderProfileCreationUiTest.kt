7b18f316e2ed721b64c0ee3a0f59144d0f47d6f1
Taush Sampley <taushsampley@gmail.com>
2024-09-21 19:59:35 -0500

    Replace manual koin setup with application setup function

--- app-shared/src/commonTest/kotlin/ride/RiderProfileCreationUiTest.kt
+++ app-shared/src/commonTest/kotlin/ride/RiderProfileCreationUiTest.kt
@@ -13,7 +13,6 @@ import androidx.compose.ui.test.onNodeWithText
 import androidx.compose.ui.test.performClick
 import androidx.compose.ui.test.performTextInput
 import androidx.compose.ui.test.runComposeUiTest
-import org.koin.core.context.startKoin
 import org.koin.core.context.stopKoin
 import org.koin.mp.KoinPlatform.getKoin
 import org.pointyware.xyz.core.navigation.StackNavigationController
@@ -21,7 +20,7 @@ import org.pointyware.xyz.core.ui.design.XyzTheme
 import org.pointyware.xyz.core.ui.di.EmptyTestUiDependencies
 import org.pointyware.xyz.feature.login.RiderProfileCreationScreen
 import org.pointyware.xyz.feature.login.viewmodels.RiderProfileCreationViewModel
-import org.pointyware.xyz.shared.di.appModule
+import org.pointyware.xyz.shared.di.setupKoin
 import kotlin.test.AfterTest
 import kotlin.test.BeforeTest
 import kotlin.test.Test
@@ -34,11 +33,7 @@ class RiderProfileCreationUiTest {
 
     @BeforeTest
     fun setUp() {
-        startKoin {
-            modules(
-                appModule()
-            )
-        }
+        setupKoin()
     }
 
     @AfterTest
