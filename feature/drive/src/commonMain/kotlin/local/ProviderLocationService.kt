6343b9bf500ca17e69f991e7a8c618a4d3291a82
Taush Sampley <taushsampley@gmail.com>
2024-09-22 15:03:16 -0500

    Add new location service

---
+++ feature/drive/src/commonMain/kotlin/local/ProviderLocationService.kt
@@ -0,0 +1,41 @@
+/*
+ * Copyright (c) 2024 Pointyware. Use of this software is governed by the GPL-3.0 license.
+ */
+
+package org.pointyware.xyz.drive.local
+
+import kotlinx.coroutines.flow.StateFlow
+import org.pointyware.xyz.core.entities.geo.Location
+
+/**
+ * Provides the current location of the driver and controls to start and stop location updates.
+ */
+interface ProviderLocationService {
+
+    /**
+     * Start location updates.
+     */
+    fun start()
+
+    /**
+     * Stop location updates.
+     */
+    fun stop()
+
+    sealed interface State {
+        val location: Location? get() = null
+        val isRunning: Boolean
+
+        data class Running(
+            override val location: Location
+        ) : State {
+            override val isRunning = true
+        }
+
+        data object Stopped : State {
+            override val isRunning = false
+        }
+    }
+
+    val state: StateFlow<State>
+}
