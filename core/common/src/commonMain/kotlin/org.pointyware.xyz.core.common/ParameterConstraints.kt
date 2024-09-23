aa412d2a27d371a4ae7276e732a7c975486f24f3
Taush Sampley <taushsampley@gmail.com>
2024-09-23 09:58:29 -0500

    Add annotation defaults

--- core/common/src/commonMain/kotlin/org.pointyware.xyz.core.common/ParameterConstraints.kt
+++ core/common/src/commonMain/kotlin/org.pointyware.xyz.core.common/ParameterConstraints.kt
@@ -8,16 +8,16 @@ package org.pointyware.xyz.core.common
  * Expresses a constraint on the length of a string.
  */
 @Target(AnnotationTarget.PROPERTY)
-annotation class StringLength(val min: Int, val max: Int)
+annotation class StringLength(val min: Int = 0, val max: Int = Int.MAX_VALUE)
 
 /**
  * Expresses a constraint on the range of an integer.
  */
 @Target(AnnotationTarget.PROPERTY)
-annotation class IntRange(val min: Int, val max: Int)
+annotation class IntRange(val min: Int = Int.MIN_VALUE, val max: Int = Int.MAX_VALUE)
 
 /**
  * Expresses a regular expression constraint on a string.
  */
 @Target(AnnotationTarget.PROPERTY)
-annotation class Regex(val pattern: String)
+annotation class Regex(val pattern: String = "^.*$")
