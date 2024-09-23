8c6e0dc9dd1d8938f4300407ab55e8879a9544e6
Taush Sampley <taushsampley@gmail.com>
2024-09-22 19:38:06 -0500

    Fix location service instance not shared

--- core/local/src/commonMain/kotlin/di/LocalModule.kt
+++ core/local/src/commonMain/kotlin/di/LocalModule.kt
@@ -7,7 +7,7 @@ package org.pointyware.xyz.core.local.di
 import kotlinx.io.files.Path
 import kotlinx.io.files.SystemFileSystem
 import org.koin.core.module.dsl.bind
-import org.koin.core.module.dsl.factoryOf
+import org.koin.core.module.dsl.singleOf
 import org.koin.core.qualifier.named
 import org.koin.dsl.module
 import org.pointyware.xyz.core.local.org.pointyware.xyz.core.local.LocationService
@@ -27,7 +27,7 @@ fun coreLocalModule() = module {
  * Defines dependencies for the core local module during tests.
  */
 fun coreLocalTestModule() = module {
-    factoryOf(::TestLocationService) { bind<LocationService>() }
+    singleOf(::TestLocationService) { bind<LocationService>() }
     factory<Path>(qualifier = testDirectory) {
         Path("testing").also {
             SystemFileSystem.createDirectories(it)
