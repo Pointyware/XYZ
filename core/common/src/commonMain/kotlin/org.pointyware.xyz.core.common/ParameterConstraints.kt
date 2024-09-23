4555104526d52bb11633662595bac990406387e7
Taush Sampley <taushsampley@gmail.com>
2024-09-23 09:55:51 -0500

    Specify annotation targets

--- core/common/src/commonMain/kotlin/org.pointyware.xyz.core.common/ParameterConstraints.kt
+++ core/common/src/commonMain/kotlin/org.pointyware.xyz.core.common/ParameterConstraints.kt
@@ -7,14 +7,17 @@ package org.pointyware.xyz.core.common
 /**
  * Expresses a constraint on the length of a string.
  */
+@Target(AnnotationTarget.PROPERTY)
 annotation class StringLength(val min: Int, val max: Int)
 
 /**
  * Expresses a constraint on the range of an integer.
  */
+@Target(AnnotationTarget.PROPERTY)
 annotation class IntRange(val min: Int, val max: Int)
 
 /**
  * Expresses a regular expression constraint on a string.
  */
+@Target(AnnotationTarget.PROPERTY)
 annotation class Regex(val pattern: String)
