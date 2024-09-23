9483a2c5311a84c0c8dc293ed57d0f24aa33e1d4
Taush Sampley <taushsampley@gmail.com>
2024-09-22 22:42:48 -0500

    Create :feature:ride test koin extensions

---
+++ feature/ride/src/commonTest/kotlin/org.pointyware.xyz.feature.ride/test/KoinExt.kt
@@ -0,0 +1,33 @@
+/*
+ * Copyright (c) 2024 Pointyware. Use of this software is governed by the GPL-3.0 license.
+ */
+
+package org.pointyware.xyz.feature.ride.test
+
+import org.koin.core.context.startKoin
+import org.pointyware.xyz.core.data.di.coreDataModule
+import org.pointyware.xyz.core.entities.di.coreEntitiesModule
+import org.pointyware.xyz.core.interactors.di.coreInteractorsModule
+import org.pointyware.xyz.core.navigation.di.coreNavigationModule
+import org.pointyware.xyz.core.ui.di.coreUiModule
+import org.pointyware.xyz.core.viewmodels.di.coreViewModelsModule
+import org.pointyware.xyz.feature.ride.di.featureRideModule
+
+
+/**
+ *
+ */
+fun setupKoin() {
+    startKoin {
+        modules(
+            coreUiModule(),
+            coreViewModelsModule(),
+            coreInteractorsModule(),
+            coreDataModule(),
+            coreEntitiesModule(),
+            coreNavigationModule(),
+
+            featureRideModule(),
+        )
+    }
+}
