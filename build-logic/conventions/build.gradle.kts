358953ca97f892a3ce29d1a5caee6a51a2db6551
Taush Sampley <taushsampley@gmail.com>
2025-04-25 03:03:03 -0500

    remove version strings

--- build-logic/conventions/build.gradle.kts
+++ build-logic/conventions/build.gradle.kts
@@ -21,13 +21,11 @@ gradlePlugin {
     plugins {
         create("koin-dependency-injection-convention") {
             id = "org.pointyware.xyz.koin-dependency-injection-convention"
-            version = "0.1.0"
             implementationClass = "org.pointyware.xyz.buildlogic.KoinDependencyInjectionConventionPlugin"
         }
 
         create("kmp-convention") {
             id = "org.pointyware.xyz.kmp-convention"
-            version = "0.1.0"
             implementationClass = "org.pointyware.xyz.buildlogic.KmpTargetsConventionPlugin"
         }
     }
