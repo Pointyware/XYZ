434d70589de0b590785a37ac3ec8137c994e2098
Taush Sampley <taushsampley@gmail.com>
2025-05-03 21:12:59 -0500

    Move AdsController to package folder

--- core/ui/src/commonMain/kotlin/org/pointyware/xyz/ui/ads/AdsController.kt
+++ core/ui/src/commonMain/kotlin/org/pointyware/xyz/ui/ads/AdsController.kt
@@ -2,7 +2,7 @@
  * Copyright (c) 2024 Pointyware. Use of this software is governed by the GPL-3.0 license.
  */
 
-package org.pointyware.xyz.core.ui.ads
+package org.pointyware.xyz.ui.ads
 
 /**
  * Abstracts Ad logic.
