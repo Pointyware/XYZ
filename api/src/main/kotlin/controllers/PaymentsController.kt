b6b9e5f91cfa6a71dfaaf82b1f1f1495a32da2c3
Taush Sampley <taushsampley@gmail.com>
2025-05-03 23:19:23 -0500

    Move payment logic out to payments controller

--- api/src/main/kotlin/controllers/PaymentsController.kt
+++ api/src/main/kotlin/controllers/PaymentsController.kt
@@ -1,17 +1,45 @@
 package org.pointyware.xyz.api.controllers
 
+import com.stripe.model.PaymentIntent
+import com.stripe.param.PaymentIntentCreateParams
+import org.pointyware.xyz.api.services.RideService
+
 /**
  *
  */
 interface PaymentsController {
+    suspend fun getRideCost(rideId: String): Result<Long>
 
+    /**
+     * Creates a payment intent for the given ride ID.
+     * @return The client secret of the payment intent.
+     */
+    suspend fun createPaymentIntent(rideId: String): Result<String>
 }
 
 /**
  *
  */
 class StripePaymentsController(
-
+    val rideService: RideService
 ): PaymentsController {
 
+    override suspend fun getRideCost(rideId: String): Result<Long> {
+        return rideService.getRideCost(rideId)
+    }
+
+    override suspend fun createPaymentIntent(rideId: String): Result<String> {
+        return rideService.getRideById(rideId)
+            .map {
+                val paymentIntentCreateParams = PaymentIntentCreateParams.builder()
+                    .setCustomer(it.riderId) // customer ID
+                    .setAmount(it.cost) // amount in cents
+                    .setCurrency("usd") // currency
+                    .build()
+
+                val intent = PaymentIntent.create(paymentIntentCreateParams)
+
+                intent.clientSecret
+            }
+    }
 }
