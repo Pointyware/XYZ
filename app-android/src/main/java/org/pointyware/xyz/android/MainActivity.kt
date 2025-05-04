3ae668fd4b9adb0ed4b735e6df11b3e4eec3ca78
Taush Sampley <taushsampley@gmail.com>
2025-05-03 19:45:58 -0500

    Move default marker to middle of atlantic ocean

--- app-android/src/main/java/org/pointyware/xyz/android/MainActivity.kt
+++ app-android/src/main/java/org/pointyware/xyz/android/MainActivity.kt
@@ -32,12 +32,10 @@ class MainActivity : FragmentActivity() {
 
             AndroidFragment<SupportMapFragment> { mapFragment ->
                 mapFragment.getMapAsync { googleMap ->
-                    val mMap = googleMap
-
-                    // Add a marker in Sydney and move the camera
-                    val sydney = LatLng(-34.0, 151.0)
-                    mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-                    mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+                    // Add a marker in the middle of the Atlantic Ocean
+                    val sydney = LatLng(34.4069, -40.8707)
+                    googleMap.addMarker(MarkerOptions().position(sydney).title("Marker in Atlantic Ocean"))
+                    googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
                 }
             }
         }
