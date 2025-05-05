a105ba33248807676f8d650f643dda6717180210
Taush Sampley <taushsampley@gmail.com>
2025-05-05 12:22:21 -0500

    Move stripe-specific logic out to StripeService

---
+++ api/src/main/kotlin/services/StripeService.kt
@@ -0,0 +1,18 @@
+package org.pointyware.xyz.api.services
+
+import com.stripe.model.PaymentIntent
+import com.stripe.param.PaymentIntentCreateParams
+
+class StripeService {
+    fun createPaymentIntent(rideId: String, cost: Long): String {
+        val paymentIntentCreateParams = PaymentIntentCreateParams.builder()
+            .setCustomer(rideId) // customer ID
+            .setAmount(cost) // amount in cents
+            .setCurrency("usd") // currency
+            .build()
+
+        val intent = PaymentIntent.create(paymentIntentCreateParams)
+
+        return intent.clientSecret
+    }
+}
