ea55edfb59c6961ec85d6c80555d114a006aa210
Taush Sampley <taushsampley@gmail.com>
2025-05-05 09:37:11 -0500

    Add documentation and utility functions to support project status

--- build-logic/conventions/src/main/kotlin/org/pointyware/xyz/build/BuildStatus.kt
+++ build-logic/conventions/src/main/kotlin/org/pointyware/xyz/build/BuildStatus.kt
@@ -1,6 +1,29 @@
 package org.pointyware.xyz.build
 
+import org.gradle.api.Project
+
+/**
+ * Defines the available values for project build status.
+ */
 enum class BuildStatus {
     Debug,
     Release
+}
+
+/**
+ * Executes the given action only if the project status is [BuildStatus.Debug].
+ */
+fun Project.debug(action: ()->Unit) {
+    if (status == BuildStatus.Debug) {
+        action()
+    }
+}
+
+/**
+ * Executes the given action only if the project status is [BuildStatus.Release].
+ */
+fun Project.release(action: ()->Unit) {
+    if (status == BuildStatus.Release) {
+        action()
+    }
 }
