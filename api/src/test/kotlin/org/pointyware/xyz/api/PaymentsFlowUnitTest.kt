0d8937282e18a46bf6cabc20852379f370424e14
Taush Sampley <taushsampley@gmail.com>
2025-05-05 15:53:08 -0500

    Stub unit test for payments flow

---
+++ api/src/test/kotlin/org/pointyware/xyz/api/PaymentsFlowUnitTest.kt
@@ -0,0 +1,37 @@
+package org.pointyware.xyz.api
+
+import kotlin.test.AfterTest
+import kotlin.test.BeforeTest
+import kotlin.test.Test
+
+
+class PaymentsFlowUnitTest {
+
+    @BeforeTest
+    fun setUp() {
+        // Setup server instance
+    }
+
+    @AfterTest
+    fun tearDown() {
+
+    }
+
+    @Test
+    fun create_client_payment_intent() {
+        // Given a completed ride ID
+
+        // When the rideId is posted to the payment intent endpoint
+
+        // Then the client secret is returned
+    }
+
+    @Test
+    fun complete_client_payment() {
+        // Given a payment intent
+
+        // When the payment is confirmed with the client secret
+
+        // Then the payment is confirmed
+    }
+}
