2fa0f89cbaef185b9de8ecd2c4f6a9640d6bbc6b
Taush Sampley <taushsampley@gmail.com>
2025-05-08 18:00:58 -0500

    Add site function docs

--- website/src/main/kotlin/org/pointyware/xyz/site/utils/Sanitization.kt
+++ website/src/main/kotlin/org/pointyware/xyz/site/utils/Sanitization.kt
@@ -2,6 +2,9 @@ package org.pointyware.xyz.site.utils
 
 /**
  * Transforms the input to conform with our rules for HTML pages.
+ *
+ * 1. An html file must end with .html
+ * 2. All whitespaces are replaced with underscores
  */
 fun sanitizeHtmlFileName(input: String): String =
     input.replace("\\s".toRegex(), "_").let {
