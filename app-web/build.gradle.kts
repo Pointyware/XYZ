c1833876a90174ed8e7f2de7d9bca1ac31e259e6
Taush Sampley <taushsampley@gmail.com>
2025-05-08 16:38:04 -0500

    remove unused imports

--- app-web/build.gradle.kts
+++ app-web/build.gradle.kts
@@ -2,12 +2,6 @@
  * Copyright (c) 2024 Pointyware. Use of this software is governed by the GPL-3.0 license.
  */
 
-import org.jetbrains.compose.ExperimentalComposeLibrary
-import org.jetbrains.kotlin.gradle.ExperimentalKotlinGradlePluginApi
-import org.jetbrains.kotlin.gradle.dsl.KotlinVersion
-import org.jetbrains.kotlin.gradle.plugin.KotlinSourceSetTree
-import org.jetbrains.kotlin.gradle.plugin.mpp.apple.XCFramework
-
 plugins {
     alias(libs.plugins.kotlinMultiplatform)
 }
