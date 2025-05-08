b8ea19f9266e8ce0ef45924332099f77a1a548d9
Taush Sampley <taushsampley@gmail.com>
2025-05-08 00:52:12 -0500

    Make RideInfo serializable

--- core-data-dtos/src/commonMain/kotlin/org/pointyware/xyz/core/data/dtos/RideInfo.kt
+++ core-data-dtos/src/commonMain/kotlin/org/pointyware/xyz/core/data/dtos/RideInfo.kt
@@ -1,8 +1,11 @@
 package org.pointyware.xyz.core.data.dtos
 
+import kotlinx.serialization.Serializable
+
 /**
  * Information about a past completed ride.
  */
+@Serializable
 data class RideInfo(
     val riderId: String,
     val cost: Long,
