e7afd0324a01ddd24c90d9cb03807fb4ed7ef950
Taush Sampley <taushsampley@gmail.com>
2025-05-08 14:18:12 -0500

    Create static website and dynamic app-web modules

---
+++ app-web/README.md
@@ -0,0 +1,4 @@
+# Module :app-web
+This module contains the web-client server for the application, or the static content.
+
+app-web - distribution is the process of delivering the final build of the API/server to some hosting endpoint, i.e., getting it to user devices, which is the same role that the Play Store and App Store serve as far as distributing a build to end users. It could be useful to develop some dsl to automate this process - the maven publishing gradle plugin already serves this purpose somewhat, with generally very fine control over authorization, supporting any service that supports the Maven publishing format.
---
+++ app-web/build.gradle.kts
@@ -0,0 +1,38 @@
+/*
+ * Copyright (c) 2024 Pointyware. Use of this software is governed by the GPL-3.0 license.
+ */
+
+import org.jetbrains.compose.ExperimentalComposeLibrary
+import org.jetbrains.kotlin.gradle.ExperimentalKotlinGradlePluginApi
+import org.jetbrains.kotlin.gradle.dsl.KotlinVersion
+import org.jetbrains.kotlin.gradle.plugin.KotlinSourceSetTree
+import org.jetbrains.kotlin.gradle.plugin.mpp.apple.XCFramework
+
+plugins {
+    alias(libs.plugins.kotlinMultiplatform)
+}
+
+kotlin {
+    jvmToolchain(21)
+
+    js() {
+        browser()
+        binaries.executable()
+    }
+
+    sourceSets {
+        val commonMain by getting {
+            dependencies {
+                implementation(libs.kotlinx.dateTime)
+                implementation(libs.kotlinx.coroutines)
+                implementation(libs.koin.core)
+            }
+        }
+        val commonTest by getting {
+            dependencies {
+                implementation(libs.kotlin.test)
+
+            }
+        }
+    }
+}
