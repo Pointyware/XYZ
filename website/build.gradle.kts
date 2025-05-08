20e6298a7179c435ffb47954204c3f7847be7f2a
Taush Sampley <taushsampley@gmail.com>
2025-05-08 14:28:46 -0500

    Enable html rendering for website module

--- website/build.gradle.kts
+++ website/build.gradle.kts
@@ -2,14 +2,9 @@
  * Copyright (c) 2024 Pointyware. Use of this software is governed by the GPL-3.0 license.
  */
 
-import org.jetbrains.compose.ExperimentalComposeLibrary
-import org.jetbrains.kotlin.gradle.ExperimentalKotlinGradlePluginApi
-import org.jetbrains.kotlin.gradle.dsl.KotlinVersion
-import org.jetbrains.kotlin.gradle.plugin.KotlinSourceSetTree
-import org.jetbrains.kotlin.gradle.plugin.mpp.apple.XCFramework
-
 plugins {
     alias(libs.plugins.kotlinJvm)
+    application
 }
 
 kotlin {
@@ -22,5 +17,12 @@ dependencies {
     implementation(libs.kotlinx.coroutines)
     implementation(libs.koin.core)
 
+    implementation(libs.kotlinx.html)
+    implementation(libs.kotlinx.css)
+
     testImplementation(libs.kotlin.test)
+}
+
+application {
+    mainClass = "org.pointyware.xyz.site.MainKt"
 }
