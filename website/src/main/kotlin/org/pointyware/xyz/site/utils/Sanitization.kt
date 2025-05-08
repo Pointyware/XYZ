19adab36c81ffdcefac74077bafb02e23c405cab
Taush Sampley <taushsampley@gmail.com>
2025-05-08 16:25:46 -0500

    Escape escape character for regex escape character....

--- website/src/main/kotlin/org/pointyware/xyz/site/utils/Sanitization.kt
+++ website/src/main/kotlin/org/pointyware/xyz/site/utils/Sanitization.kt
@@ -4,7 +4,7 @@ package org.pointyware.xyz.site.utils
  * Transforms the input to conform with our rules for HTML pages.
  */
 fun sanitizeHtmlFileName(input: String): String =
-    input.replace("\s".toRegex(), "_").let {
+    input.replace("\\s".toRegex(), "_").let {
         if (it.endsWith(".html")) {
             it
         } else {
