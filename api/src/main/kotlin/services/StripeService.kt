5505c1e0d50807f1a7a4964acfb066a750f480f2
Taush Sampley <taushsampley@gmail.com>
2025-05-08 23:00:32 -0500

    Create separation between stripe service and payments interface

--- api/src/main/kotlin/services/StripeService.kt
+++ api/src/main/kotlin/services/StripeService.kt
@@ -1,10 +1,28 @@
 package org.pointyware.xyz.api.services
 
+import com.stripe.Stripe
 import com.stripe.model.PaymentIntent
 import com.stripe.param.PaymentIntentCreateParams
 
-class StripeService {
-    fun createPaymentIntent(rideId: String, cost: Long): Result<String> = runCatching {
+/**
+ *
+ */
+interface PaymentsService {
+    fun createPaymentIntent(rideId: String, cost: Long): Result<String>
+}
+
+/**
+ * Interacts with the Stripe API to create payment intents for completed rides.
+ */
+class StripeService(
+
+): PaymentsService {
+
+    init {
+        Stripe.apiKey = System.getenv("STRIPE_API_KEY") ?: throw IllegalStateException("Stripe API key not set")
+    }
+
+    override fun createPaymentIntent(rideId: String, cost: Long): Result<String> = runCatching {
         val paymentIntentCreateParams = PaymentIntentCreateParams.builder()
             .setCustomer(rideId) // customer ID
             .setAmount(cost) // amount in cents
