bc76b5738edd291ca6d5af7f6d640d46d423bffa
Taush Sampley <taushsampley@gmail.com>
2025-05-08 20:12:05 -0500

    Create postgres connection factory

---
+++ api/src/main/kotlin/services/UserService.kt
@@ -0,0 +1,64 @@
+package org.pointyware.xyz.api.services
+
+import org.pointyware.xyz.api.model.UserCredentials
+import org.pointyware.xyz.core.data.dtos.Authorization
+import org.postgresql.Driver
+import java.sql.Connection
+import java.util.Properties
+
+/**
+ * Service for managing user credentials and authorizations.
+ */
+interface UserService {
+
+    /**
+     * Fetches the user credentials for the given email.
+     * @throws MissingCredentialsException if the email is invalid.
+     */
+    suspend fun getUserCredentials(email: String): UserCredentials
+    suspend fun generateAuthorization(email: String): Authorization
+
+    class MissingCredentialsException(val email: String) : Exception("Missing credentials for email: $email")
+}
+
+/**
+ *
+ */
+class PostgresUserService(
+    private val encryptionService: EncryptionService,
+    private val connection: Connection
+) : UserService {
+
+    override suspend fun getUserCredentials(email: String): UserCredentials {
+        val userCredentials: UserCredentials = connection.prepareStatement(
+            "SELECT * FROM user_credentials WHERE email = ?"
+        ).apply {
+            setString(1, email)
+        }.executeQuery().use { resultSet ->
+            if (resultSet.next()) {
+                UserCredentials(
+                    email = resultSet.getString("email"),
+                    hash = resultSet.getString("hash"),
+                    salt = resultSet.getString("salt")
+                )
+            } else {
+                throw UserService.MissingCredentialsException(email)
+            }
+        }
+        return userCredentials
+    }
+
+    override suspend fun generateAuthorization(email: String): Authorization {
+        val authorization = Authorization(
+            email = email,
+            token = encryptionService.generateToken(email)
+        )
+        connection.prepareStatement(
+            "INSERT INTO authorizations (email, token) VALUES (?, ?)"
+        ).apply {
+            setString(1, email)
+            setString(2, authorization.token)
+        }.executeUpdate()
+        return authorization
+    }
+}
