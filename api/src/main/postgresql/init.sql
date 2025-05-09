4783f294fa3a0dcf7ce66a143651effbdc25c2ce
Taush Sampley <taushsampley@gmail.com>
2025-05-08 20:44:31 -0500

    Create sql file to document database layout

---
+++ api/src/main/postgresql/init.sql
@@ -0,0 +1,18 @@
+CREATE TABLE users(
+    id INT PRIMARY KEY,
+    email TEXT NOT NULL,
+    pass_hash TEXT NOT NULL,
+    salt TEXT NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE user_permissions(
+    user_id INT FOREIGN KEY REFERENCES users(id),
+    permission TEXT NOT NULL
+);
+
+CREATE TABLE authorizations(
+    email TEXT NOT NULL,
+    token TEXT NOT NULL
+);
