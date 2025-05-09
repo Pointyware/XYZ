3c895ff0f33fe2474691000cc81fbc70b36573ac
Taush Sampley <taushsampley@gmail.com>
2025-05-08 20:38:13 -0500

    Create trivial encryption service test

--- api/src/test/kotlin/org/pointyware/xyz/api/services/EncryptionServiceTest.kt
+++ api/src/test/kotlin/org/pointyware/xyz/api/services/EncryptionServiceTest.kt
@@ -3,12 +3,15 @@ package org.pointyware.xyz.api.services
 import kotlin.test.AfterTest
 import kotlin.test.BeforeTest
 import kotlin.test.Test
+import kotlin.test.assertFails
+import kotlin.test.assertNotEquals
 
 class EncryptionServiceTest {
 
+    private lateinit var serviceUnderTest: EncryptionService
     @BeforeTest
     fun setUp() {
-
+        serviceUnderTest = EncryptionServiceImpl()
     }
 
     @AfterTest
@@ -17,9 +20,23 @@ class EncryptionServiceTest {
     }
 
     @Test
-    fun encryption_service_initializes_cipher() {
-        val service = EncryptionServiceImpl()
+    fun salted_password() {
+        val hash = serviceUnderTest.saltedHash("password", "salt")
 
+        assertNotEquals("", hash)
+    }
 
+    @Test
+    fun generate_token_fails_for_unknown_user() {
+        assertFails {
+            serviceUnderTest.generateToken(email = "some-unknown@some.maiil", resourcePermissions = emptyList())
+        }
+    }
+
+    @Test
+    fun generate_token_passes_for_known_user() {
+        val token = serviceUnderTest.generateToken(email = "user_email@some.mail", resourcePermissions = emptyList())
+
+        assertNotEquals("", token)
     }
 }
