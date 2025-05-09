c35340c16d894b4e0eddeb71c0e99145c1ebaed0
Taush Sampley <taushsampley@gmail.com>
2025-05-08 20:00:30 -0500

    Create EncryptionService and test

---
+++ api/src/test/kotlin/org/pointyware/xyz/api/services/EncryptionServiceTest.kt
@@ -0,0 +1,25 @@
+package org.pointyware.xyz.api.services
+
+import kotlin.test.AfterTest
+import kotlin.test.BeforeTest
+import kotlin.test.Test
+
+class EncryptionServiceTest {
+
+    @BeforeTest
+    fun setUp() {
+
+    }
+
+    @AfterTest
+    fun tearDown() {
+
+    }
+
+    @Test
+    fun encryption_service_initializes_cipher() {
+        val service = EncryptionServiceImpl()
+
+
+    }
+}
