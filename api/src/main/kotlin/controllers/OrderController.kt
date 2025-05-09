de3cad57a01c62937ed6955ac9ab960a5b0a51a4
Taush Sampley <taushsampley@gmail.com>
2025-05-08 22:41:04 -0500

    Provide stubbed order controller implementation

--- api/src/main/kotlin/controllers/OrderController.kt
+++ api/src/main/kotlin/controllers/OrderController.kt
@@ -1,5 +1,7 @@
 package org.pointyware.xyz.api.controllers
 
+import org.pointyware.xyz.api.services.RideService
+
 /**
  * Controls the market orders.
  */
@@ -12,3 +14,20 @@ interface OrderController {
 interface BidResponse
 interface AskResponse
 interface FairMarketValueResponse
+
+class OrderControllerImpl(
+    private val rideService: RideService
+): OrderController {
+
+    override suspend fun bid(moneys: Float): Result<BidResponse> {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun ask(moneys: Float): Result<AskResponse> {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun getFairMarketValue(): Result<FairMarketValueResponse> {
+        TODO("Not yet implemented")
+    }
+}
