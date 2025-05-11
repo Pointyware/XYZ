7f9f86db22f58e77b7b6e15deb61cd31a0af4959
Taush Sampley <taushsampley@gmail.com>
2025-05-11 14:40:07 -0500

    Create AuthDatabaseIntegrationTest

---
+++ api/src/test/kotlin/org/pointyware/xyz/api/databases/AuthDatabaseIntegrationTest.kt
@@ -0,0 +1,20 @@
+package org.pointyware.xyz.api.databases
+
+/**
+ *  This test exercises the [AuthDatabaseImpl] against a real PostgreSQL database.
+ */
+class AuthDatabaseIntegrationTest {
+
+    /*
+    @get:Rule
+    val dbRule = DatabaseRule()
+
+    PostgreSQLContainer<Nothing>("postgres:latest")
+        .withDatabaseName("testdb")
+        .withUsername("user")
+        .withPassword("password")
+        .withExposedPorts(5432)
+        .withNetworkAliases("testdb")
+     */
+
+}
---
+++ api/src/test/kotlin/org/pointyware/xyz/api/databases/README.md
@@ -0,0 +1,15 @@
+# Package org.pointyware.xyz.api.databases
+This test package includes database integration tests which must be run against a running
+postgres database. To start the database, 
+
+```shell
+initdb -D /usr/local/pgsql/data
+postgres -D /usr/local/pgsql/data
+  > logfile 2>&1 &
+```
+
+```shell
+pg_ctl start -l logfile -D /usr/local/pgsql/data
+```
+
+The argument -D can be replaced by setting the environment variable PGDATA.
