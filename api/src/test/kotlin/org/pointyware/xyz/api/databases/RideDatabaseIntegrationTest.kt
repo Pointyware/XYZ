11733a800302cbb8582d9e3f9abe0e9a7b71bfeb
Taush Sampley <taushsampley@gmail.com>
2025-05-11 14:47:27 -0500

    Create empty stubbed data base integration tests

---
+++ api/src/test/kotlin/org/pointyware/xyz/api/databases/RideDatabaseIntegrationTest.kt
@@ -0,0 +1,54 @@
+package org.pointyware.xyz.api.databases
+
+import kotlin.test.AfterTest
+import kotlin.test.BeforeTest
+import kotlin.test.Test
+
+
+class RideDatabaseIntegrationTest {
+
+    @BeforeTest
+    fun setUp() {
+
+    }
+
+    @AfterTest
+    fun tearDown() {
+
+    }
+
+    @Test
+    fun driver_go_active() {
+
+    }
+
+    @Test
+    fun rider_hail_ride() {
+
+    }
+
+    @Test
+    fun driver_accept_ride() {
+
+    }
+
+    @Test
+    fun driver_arrive_at_pickup() {
+
+    }
+
+    @Test
+    fun driver_start_ride() {
+
+    }
+
+    @Test
+    fun driver_arrive_at_destination() {
+
+    }
+
+    @Test
+    fun driver_complete_ride() {
+
+    }
+}
