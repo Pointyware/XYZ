dc3afda46bb5d07d5f33e7c1675cb3d4bb584909
Taush Sampley <taushsampley@gmail.com>
2025-05-11 17:44:58 -0500

    Move auth_init into admin/init.sql

--- api/src/main/resources/postgresql/admin/init.sql
+++ api/src/main/resources/postgresql/admin/init.sql
@@ -1,6 +1,26 @@
--- Initializes entire database from scratch
 CREATE USER xyz_api;
 
 CREATE DATABASE xyz;
-
 GRANT CONNECT ON DATABASE xyz TO xyz_api;
+
+CREATE TABLE xyz.users(
+    id INT PRIMARY KEY,
+    email TEXT NOT NULL,
+    pass_hash TEXT NOT NULL,
+    salt TEXT NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+GRANT ALL ON TABLE xyz.users TO xyz_api;
+
+CREATE TABLE xyz.user_permissions(
+    user_id INT REFERENCES users(id),
+    permission TEXT NOT NULL
+);
+GRANT ALL ON TABLE xyz.user_permissions TO xyz_api;
+
+CREATE TABLE xyz.authorizations(
+    email TEXT NOT NULL,
+    token TEXT NOT NULL
+);
+GRANT ALL ON TABLE xyz.authorizations TO xyz_api;
