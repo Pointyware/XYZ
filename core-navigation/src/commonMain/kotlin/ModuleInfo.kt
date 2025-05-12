9a3af925be9282098ea8dc030237e3136083e905
Taush Sampley <taushsampley@gmail.com>
2025-05-12 16:51:35 -0500

    Remove in-house navigation components

--- core-navigation/src/commonMain/kotlin/ModuleInfo.kt
+++
@@ -1,7 +0,0 @@
-/*
- * Copyright (c) 2024 Pointyware. Use of this software is governed by the GPL-3.0 license.
- */
-
-package org.pointyware.xyz.core.navigation
-
-const val MODULE_NAME = ":core:navigation"
