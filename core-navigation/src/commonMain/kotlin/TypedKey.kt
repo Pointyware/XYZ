8185a8596353c17f8a8ff9e95aead088c3a21210
Taush Sampley <taushsampley@gmail.com>
2025-05-12 16:51:35 -0500

    Remove in-house navigation components

--- core-navigation/src/commonMain/kotlin/TypedKey.kt
+++
@@ -1,9 +0,0 @@
-package org.pointyware.xyz.core.navigation
-
-data class TypedKey<T>(
-    val key: String
-)
-
-fun <T> String.toTypedKey(): TypedKey<T> {
-    return TypedKey(this)
-}
---
+++ core-navigation/src/commonMain/kotlin/org/pointyware/xyz/core/navigation/di/NavigationModule.kt
@@ -0,0 +1,16 @@
+/*
+ * Copyright (c) 2024 Pointyware. Use of this software is governed by the GPL-3.0 license.
+ */
+
+package org.pointyware.xyz.core.navigation.di
+
+import org.koin.core.qualifier.named
+import org.koin.dsl.module
+
+val homeQualifier = named("home")
+/**
+ *
+ */
+fun coreNavigationModule() = module {
+
+}
