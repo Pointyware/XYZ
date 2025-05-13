b544e3c1111628674194e218d53cf96e143b6e36
Taush Sampley <taushsampley@gmail.com>
2025-05-13 17:49:57 -0500

    Nest routing

--- api/src/main/kotlin/routes/DriveRouting.kt
+++ api/src/main/kotlin/routes/DriveRouting.kt
@@ -7,15 +7,32 @@ package org.pointyware.xyz.api.routes
 import io.ktor.server.routing.Routing
 import io.ktor.server.routing.post
 import io.ktor.server.routing.put
+import io.ktor.server.routing.route
 import org.koin.mp.KoinPlatform.getKoin
+import org.pointyware.xyz.api.controllers.OrderController
+import org.pointyware.xyz.api.controllers.RideController
 
 /**
- * Routes drive endpoint requests to the appropriate controller.
+ * Routes drive endpoint requests to the appropriate controller. Drive endpoints are meant
+ * for driver use only.
  */
 fun Routing.drive() {
     val koin = getKoin()
-    post("/drive/start") {}
-    put("/drive/status") {}
-    post("/drive/accept") {}
-    post("/drive/stop") {}
+    route("/drive") {
+        val orderController = koin.get<OrderController>()
+        val rideController = koin.get<RideController>()
+
+        post("/start") {
+            // TODO: get bid info
+        }
+        put("/status") {
+            // TODO: get bid update
+        }
+        post("/accept") {
+
+        }
+        post("/stop") {
+
+        }
+    }
 }
