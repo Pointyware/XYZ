f079aa08a1c0a01e132b45d7e7bfa32d5d1ba8cb
Taush Sampley <taushsampley@gmail.com>
2025-05-13 17:20:38 -0500

    Add RideController

---
+++ api/src/main/kotlin/controllers/RideController.kt
@@ -0,0 +1,40 @@
+package org.pointyware.xyz.api.controllers
+
+import org.pointyware.xyz.api.services.RideServiceImpl
+import org.pointyware.xyz.core.entities.ride.Ride
+
+/**
+ * Once a bid/ask has been settled through the [OrderController], the ride controller manages
+ * the state and lifecycles of the ride.
+ */
+interface RideController {
+    suspend fun startRide(): Result<Ride>
+    suspend fun stopRide(): Result<Ride>
+    suspend fun getRideStatus(): Result<Ride>
+}
+
+/**
+ *
+ */
+class RideControllerImpl(
+    private val rideService: RideServiceImpl
+): RideController {
+
+    /**
+     * Called by driver to start a ride.
+     */
+    override suspend fun startRide(): Result<Ride> {
+        TODO("Not yet implemented")
+    }
+
+    /**
+     *
+     */
+    override suspend fun stopRide(): Result<Ride> {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun getRideStatus(): Result<Ride> {
+        TODO("Not yet implemented")
+    }
+}
