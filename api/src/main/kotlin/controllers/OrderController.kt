d555590cc7f2c9ebef000361113fcacd17234379
Taush Sampley <taushsampley@gmail.com>
2025-05-13 18:04:45 -0500

    replace moneys float with cents long

--- api/src/main/kotlin/controllers/OrderController.kt
+++ api/src/main/kotlin/controllers/OrderController.kt
@@ -6,8 +6,8 @@ import org.pointyware.xyz.api.services.RideService
  * Controls the market orders.
  */
 interface OrderController {
-    suspend fun bid(moneys: Float): Result<BidResponse>
-    suspend fun ask(moneys: Float): Result<AskResponse>
+    suspend fun bid(cents: Long): Result<BidResponse>
+    suspend fun ask(cents: Long): Result<AskResponse>
     suspend fun getFairMarketValue(): Result<FairMarketValueResponse>
 }
 
@@ -15,15 +15,18 @@ interface BidResponse
 interface AskResponse
 interface FairMarketValueResponse
 
+/**
+ * Interacts with the Stripe API to create payment intents for completed rides.
+ */
 class OrderControllerImpl(
     private val rideService: RideService
 ): OrderController {
 
-    override suspend fun bid(moneys: Float): Result<BidResponse> {
+    override suspend fun bid(cents: Long): Result<BidResponse> {
         TODO("Not yet implemented")
     }
 
-    override suspend fun ask(moneys: Float): Result<AskResponse> {
+    override suspend fun ask(cents: Long): Result<AskResponse> {
         TODO("Not yet implemented")
     }
 
