1ac7d92cd91858b1282bea785375308ec2fbb053
Taush Sampley <taushsampley@gmail.com>
2025-05-15 18:28:57 -0500

    move respondResult utility out to dedicated file

---
+++ api/src/main/kotlin/routes/ApplicationCallExt.kt
@@ -0,0 +1,17 @@
+package org.pointyware.xyz.api.routes
+
+import io.ktor.http.HttpStatusCode
+import io.ktor.server.application.ApplicationCall
+import io.ktor.server.response.respond
+import io.ktor.server.response.respondText
+
+
+/**
+ * Passes the result along as a 200 OK response if successful, or a 500 Internal Server Error if not.
+ */
+suspend inline fun <reified T : Any> ApplicationCall.respondResult(result: Result<T>) {
+    result
+        .onSuccess { respond(it) }
+        .onFailure { error -> respondText(error.message?.let { error.toString() + it } ?: "Unknown error", status = HttpStatusCode.InternalServerError) }
+    respondText("Unreachable", status = HttpStatusCode.InternalServerError)
+}
