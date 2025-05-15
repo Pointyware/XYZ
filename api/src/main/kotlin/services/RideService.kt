95b3e971b600e231aa1d1ecc497d0975cef0a768
Taush Sampley <taushsampley@gmail.com>
2025-05-15 16:56:23 -0500

    Remove getRidecost

--- api/src/main/kotlin/services/RideService.kt
+++ api/src/main/kotlin/services/RideService.kt
@@ -5,17 +5,16 @@ import org.pointyware.xyz.core.data.dtos.RideInfo
 
 interface RideService {
     suspend fun getRideById(id: String): Result<RideInfo>
-    suspend fun getRideCost(id: String): Result<Long>
 }
 
 class RideServiceImpl(
     private val rideDatabase: RideDatabase
 ): RideService {
-    override suspend fun getRideById(id: String): Result<RideInfo> {
-        return TODO("")
-    }
-
-    override suspend fun getRideCost(id: String): Result<Long> {
-        return TODO("")
+    override suspend fun getRideById(id: String): Result<RideInfo> = runCatching {
+        val ride = rideDatabase.rides.getRideById(id)
+        RideInfo(
+            riderId = ride.id,
+            cost = ride.cost
+        )
     }
 }
