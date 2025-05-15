83a778af494083bc61f0ee7d8146a327b1eb083b
Taush Sampley <taushsampley@gmail.com>
2025-05-14 22:58:30 -0500

    Replace email ids with uuid

--- api/src/main/kotlin/services/EncryptionService.kt
+++ api/src/main/kotlin/services/EncryptionService.kt
@@ -1,10 +1,13 @@
 package org.pointyware.xyz.api.services
 
 import javax.crypto.Cipher
+import kotlin.uuid.ExperimentalUuidApi
+import kotlin.uuid.Uuid
 
 /**
  * Provides access to encryption and hashing functions.
  */
+@OptIn(ExperimentalUuidApi::class)
 interface EncryptionService {
     /**
      *  Generates a random salt value.
@@ -21,13 +24,14 @@ interface EncryptionService {
      * implementation dependent, but usually come in a form like "api_bid:read,write;api_ask:read"
      * or abbreviated forms like "bid:rw;ask:r".
      */
-    fun generateToken(email: String, resourcePermissions: List<String>): Result<String>
+    fun generateToken(uuid: Uuid, resourcePermissions: List<String>): Result<String>
 }
 
 /**
  * Implements [EncryptionService] using the Java Cryptography Architecture (JCA) and the
  * Java Cryptography Extension (JCE).
  */
+@OptIn(ExperimentalUuidApi::class)
 class EncryptionServiceImpl(
     // TODO: inject JVM encryption dependencies
 ): EncryptionService {
@@ -49,12 +53,12 @@ class EncryptionServiceImpl(
         hash.toHexString()
     }
 
-    override fun generateToken(email: String, resourcePermissions: List<String>): Result<String> = runCatching {
-        val emailHash = asymmetricCipher.doFinal(email.toByteArray())
+    override fun generateToken(uuid: Uuid, resourcePermissions: List<String>): Result<String> = runCatching {
+        val idHash = asymmetricCipher.doFinal(uuid.toByteArray())
         val permissionString = resourcePermissions.joinToString(",")
         val permissionHash = asymmetricCipher.doFinal(permissionString.toByteArray())
 
-        emailHash.toHexString() + "." + permissionHash.toHexString()
+        idHash.toHexString() + "." + permissionHash.toHexString()
     }
 
     private fun ByteArray.toHexString() = this.joinToString("") { String.format("%02x", it) }
