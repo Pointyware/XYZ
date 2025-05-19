75a2e5e9d9a640a2b341cda9de2fdf8289197913
Taush Sampley <taushsampley@gmail.com>
2025-05-19 00:39:52 -0500

    remove trailing old db init

--- api/src/main/resources/postgresql/init.sql
+++ api/src/main/resources/postgresql/init.sql
@@ -313,29 +313,3 @@ $$ LANGUAGE plpgsql SECURITY DEFINER;
 CREATE TRIGGER users_audit_trigger
 AFTER INSERT OR UPDATE OR DELETE ON auth.users
 FOR EACH ROW EXECUTE FUNCTION audit.log_changes();
-
-
-CREATE DATABASE xyz;
-GRANT CONNECT ON DATABASE xyz TO xyz_api;
-
-CREATE TABLE xyz.users(
-    id INT PRIMARY KEY,
-    email TEXT NOT NULL,
-    pass_hash TEXT NOT NULL,
-    salt TEXT NOT NULL,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
-GRANT ALL ON TABLE xyz.users TO xyz_api;
-
-CREATE TABLE xyz.user_permissions(
-    user_id INT REFERENCES users(id),
-    permission TEXT NOT NULL
-);
-GRANT ALL ON TABLE xyz.user_permissions TO xyz_api;
-
-CREATE TABLE xyz.authorizations(
-    email TEXT NOT NULL,
-    token TEXT NOT NULL
-);
-GRANT ALL ON TABLE xyz.authorizations TO xyz_api;
