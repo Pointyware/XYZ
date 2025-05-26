b12398c970feec0ebace0c7416abe88473e1945e
Taush Sampley <taushsampley@gmail.com>
2025-05-25 20:22:06 -0500

    Allow command arguments to control server port
    - also extensible for other command options

--- api/src/main/kotlin/org/pointyware/xyz/api/Server.kt
+++ api/src/main/kotlin/org/pointyware/xyz/api/Server.kt
@@ -19,9 +19,18 @@ import org.pointyware.xyz.api.routes.payment
 import org.pointyware.xyz.api.routes.profile
 import org.pointyware.xyz.api.routes.ride
 
+/**
+ * Main entry point for the XYZ API server.
+ *
+ * Consumes command line arguments to determine the port to run on. Default is 80.
+ * ```shell
+ * java -jar xyz-api.jar --port=<port>
+ * ```
+ */
 fun main(vararg args: String) {
-    // TODO: take port from args
-    val port = 80
+    var programInputs = ProgramInputs(port = 80)
+
+    programInputs = args.iterator().processArgs(programInputs)
 
     startKoin {
         modules(
@@ -29,7 +38,7 @@ fun main(vararg args: String) {
         )
     }
 
-    embeddedServer(Netty, port) {
+    embeddedServer(Netty, programInputs.port) {
         install(ContentNegotiation) {
             json(Json {
                 prettyPrint = true
@@ -46,4 +55,59 @@ fun main(vararg args: String) {
             payment()
         }
     }.start(wait = true)
+}
+
+enum class CommandOption(
+    val shortName: String,
+    val longName: String,
+    val onShortMatch: (Iterator<String>, ProgramInputs) -> ProgramInputs,
+    val onLongMatch: (String, Iterator<String>, ProgramInputs) -> ProgramInputs
+) {
+    Port(
+        "p",
+        "port",
+        onShortMatch = { args, inputs ->
+            inputs.copy(port = args.next().toInt())
+        },
+        onLongMatch = { arg, args, inputs ->
+            val longPrefix = "--${Port.longName}="
+            when {
+                arg == Port.longName -> {
+                    inputs.copy(port = args.next().toInt())
+                }
+                arg.startsWith(longPrefix) -> {
+                    inputs.copy(port = arg.substringAfter(longPrefix).toInt())
+                }
+                else -> throw IllegalArgumentException("Expected `--port <port>` or `--port=<port>`, but got `$arg`")
+            }
+        }
+    )
+}
+
+data class ProgramInputs(
+    val port: Int
+)
+
+private fun Iterator<String>.processArgs(inputs: ProgramInputs): ProgramInputs {
+    var argState = inputs
+
+    argLoop@
+    while (hasNext()) {
+        val arg = next()
+        for (it in CommandOption.entries) {
+            when {
+                arg == it.shortName -> {
+                    argState = it.onShortMatch(this, argState)
+                    continue@argLoop
+                }
+                arg.startsWith(it.longName) -> {
+                    argState = it.onLongMatch(arg, this, argState)
+                    continue@argLoop
+                }
+            }
+        }
+        throw IllegalArgumentException("Unknown argument: $arg. Expected one of: ${CommandOption.entries.joinToString { it.longName }}")
+    }
+
+    return argState
 }
