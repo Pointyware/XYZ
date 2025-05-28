9f40b88e96f064ec7ad27945ae862925dc154a3f
Taush Sampley <taushsampley@gmail.com>
2025-05-28 00:08:41 -0500

    Add session auth controller functions

--- api/src/main/kotlin/org/pointyware/xyz/api/controllers/AuthController.kt
+++ api/src/main/kotlin/org/pointyware/xyz/api/controllers/AuthController.kt
@@ -2,27 +2,38 @@ package org.pointyware.xyz.api.controllers
 
 import org.pointyware.xyz.api.services.UserService
 import org.pointyware.xyz.core.data.dtos.Authorization
+import kotlin.uuid.ExperimentalUuidApi
+import kotlin.uuid.Uuid
 
 /**
  * Controller for handling authentication-related operations that return authorization tokens.
  */
 //@AdapterLayer
+@OptIn(ExperimentalUuidApi::class)
 interface AuthController {
-    suspend fun login(email: String, password: String): Result<Authorization>
     suspend fun createUser(email: String, password: String): Result<Authorization>
+    suspend fun createSession(uuidString: String, deviceInfo: String): Result<Uuid>
+    suspend fun authenticate(email: String, password: String): Result<Authorization>
+    suspend fun validateSession(sessionId: String): Result<Unit>
 }
 
 /**
  *
  */
+@ExperimentalUuidApi
 class AuthControllerImpl(
     private val userService: UserService
 ): AuthController {
-    override suspend fun login(email: String, password: String): Result<Authorization> = runCatching {
-        userService.validateCredentials(email, password)
-    }
-
     override suspend fun createUser(email: String, password: String): Result<Authorization> = runCatching {
         userService.createUser(email, password)
+    }
+    override suspend fun createSession(uuidString: String, deviceInfo: String): Result<Uuid> {
+        TODO("Not yet implemented")
+    }
+    override suspend fun authenticate(email: String, password: String): Result<Authorization> = runCatching {
+        userService.validateCredentials(email, password)
+    }
+    override suspend fun validateSession(sessionId: String): Result<Unit> {
+        TODO("Not yet implemented")
     }
 }
