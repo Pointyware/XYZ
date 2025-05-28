732d1b6cdd72a2f80ec9b1772dc0a0db708153c2
Taush Sampley <taushsampley@gmail.com>
2025-05-28 16:48:36 -0500

    Initialize tofu providers

--- api/main.tf
+++ api/main.tf
@@ -1,15 +1,15 @@
 
-# provider "aws" {
-#   region = "us-central-1"
-# }
-#
-# provider "google" {
-#   project = "my-gcp-project"
-#   region  = "us-central1"
-# }
-#
+provider "aws" {
+  region = "us-central-1"
+}
+
+provider "google" {
+  project = "my-gcp-project"
+  region  = "us-central1"
+}
+
 # resource "aws_instance" "api_instance" {
-#   ami           = "ami-0c55b159cbfafe1f0" # Example AMI, replace with a valid one
+#   ami           = "ami-0c55b159cbfafe1f0"
 #   instance_type = "t2.micro"
 #   tags = {
 #     Name = "API Instance"
