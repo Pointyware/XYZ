96c5c17b0fc1e439ca69fa8e8364c9110eeea0a4
Taush Sampley <taushsampley@gmail.com>
2025-05-28 00:08:20 -0500

    use basic authentication to protect login and account creation resources

--- api/src/main/kotlin/org/pointyware/xyz/api/routes/AuthRouting.kt
+++ api/src/main/kotlin/org/pointyware/xyz/api/routes/AuthRouting.kt
@@ -5,12 +5,20 @@
 package org.pointyware.xyz.api.routes
 
 import io.ktor.server.application.call
+import io.ktor.server.auth.UserIdPrincipal
+import io.ktor.server.auth.authenticate
+import io.ktor.server.auth.principal
 import io.ktor.server.routing.Routing
 import io.ktor.server.routing.post
 import io.ktor.server.routing.route
+import io.ktor.server.sessions.sessions
+import io.ktor.server.sessions.set
 import org.koin.mp.KoinPlatform.getKoin
+import org.pointyware.xyz.api.basicAuthProvider
 import org.pointyware.xyz.api.controllers.AuthController
 import org.pointyware.xyz.core.data.dtos.LoginInfo
+import org.pointyware.xyz.core.data.dtos.UserSession
+import kotlin.uuid.ExperimentalUuidApi
 
 /**
  * Directs auth requests to the appropriate controller.
@@ -19,18 +27,30 @@ import org.pointyware.xyz.core.data.dtos.LoginInfo
  *
  * In the future, we will have a dedicated auth server under the subdomain `auth.pointyware.org`.
  */
+@OptIn(ExperimentalUuidApi::class)
 fun Routing.auth() {
     val koin = getKoin()
     route("/auth") {
-        post<LoginInfo>("/login") { info ->
-            val authController = koin.get<AuthController>()
+        authenticate(basicAuthProvider) {
+            post("/login") {
+                val userId = call.principal<UserIdPrincipal>()!!.name
+                // TODO: get device info from request headers or session
+                val deviceInfo = call.request.headers["User-Agent"] ?: "unknown-device"
+                val authController = koin.get<AuthController>()
+                val result = authController.createSession(userId, deviceInfo)
+                    .onSuccess {
+                        call.sessions.set(UserSession(userId = userId, sessionId = it.toHexString()))
+                    }
 
-            call.respondResult(authController.login(info.email, info.password))
+                call.respondResult(result)
+            }
         }
-        post<LoginInfo>("/create") { info ->
-            val authController = koin.get<AuthController>()
+        authenticate(basicAuthProvider) {
+            post<LoginInfo>("/create") { info ->
+                val authController = koin.get<AuthController>()
 
-            call.respondResult(authController.createUser(info.email, info.password))
+                call.respondResult(authController.createUser(info.email, info.password))
+            }
         }
     }
 }
