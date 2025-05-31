d28d911802170ea9df440dcce462b032207f992a
Taush Sampley <taushsampley@gmail.com>
2025-05-31 00:48:55 -0500

    Add orderbook streaming endpoint

--- api/src/main/kotlin/org/pointyware/xyz/api/controllers/OrderController.kt
+++ api/src/main/kotlin/org/pointyware/xyz/api/controllers/OrderController.kt
@@ -1,5 +1,7 @@
 package org.pointyware.xyz.api.controllers
 
+import kotlinx.coroutines.flow.Flow
+import kotlinx.serialization.Serializable
 import org.pointyware.xyz.api.services.RideService
 
 /**
@@ -9,11 +11,16 @@ interface OrderController {
     suspend fun bid(cents: Long): Result<BidResponse>
     suspend fun ask(cents: Long): Result<AskResponse>
     suspend fun getFairMarketValue(): Result<FairMarketValueResponse>
+    fun streamOrders(userId: String): Flow<OrderBook>
 }
 
 interface BidResponse
 interface AskResponse
 interface FairMarketValueResponse
+@Serializable
+data class OrderBook(
+    val orders: List<Long>
+)
 
 /**
  * Interacts with the Stripe API to create payment intents for completed rides.
@@ -31,6 +38,10 @@ class OrderControllerImpl(
     }
 
     override suspend fun getFairMarketValue(): Result<FairMarketValueResponse> {
+        TODO("Not yet implemented")
+    }
+
+    override fun streamOrders(userId: String): Flow<OrderBook> {
         TODO("Not yet implemented")
     }
 }
