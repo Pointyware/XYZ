d1d8e076ec7ff479c6a077f5f12a304c91a96a60
Taush Sampley <taushsampley@gmail.com>
2025-06-02 18:30:00 -0500

    Swap GPL3 for AGPL3 to protect against SaaS parisitism

--- api/src/main/resources/postgresql/rider/active_drivers.sql
+++ api/src/main/resources/postgresql/rider/active_drivers.sql
@@ -1,6 +1,6 @@
 
 /*
- * Copyright (c) 2025 Pointyware. Use of this software is governed by the GPL-3.0 license.
+ * Copyright (c) 2025 Pointyware. Use of this software is governed by the Affero GPL-3.0 license.
  */
 
 -- searchNearCoordinate(:longitude, :latitude, :radius)
--- core-entities/src/commonMain/kotlin/di/Module.kt
+++ core-entities/src/commonMain/kotlin/di/Module.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2024-2025 Pointyware. Use of this software is governed by the GPL-3.0 license.
+ * Copyright (c) 2024-2025 Pointyware. Use of this software is governed by the Affero GPL-3.0 license.
  */
 
 package org.pointyware.xyz.core.entities.di
